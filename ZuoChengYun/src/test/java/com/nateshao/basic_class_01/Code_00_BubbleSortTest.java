6fe96397c24626f0f089d4cf5e210fe84f8b7c75
nateshao <shaotongjie@gmail.com>
2021-09-12 13:39:40 +0800

    代码整理

---
+++ ZuoChengYun/src/test/java/com/nateshao/basic_class_01/Code_00_BubbleSortTest.java
@@ -0,0 +1,77 @@
+package com.nateshao.basic_class_01;
+
+import org.junit.Test;
+import java.util.Arrays;
+/**
+ * @date Created by 邵桐杰 on 2021/9/11 16:35
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Code_00_BubbleSortTest {
+
+    @Test
+    public void bubbleSortTest() {
+        int[] arr = {1, 9, 4, 8, 2, 6};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    @Test
+    public void swapTest() {
+    }
+
+    @Test
+    public void comparator() {
+    }
+
+    @Test
+    public void generateRandomArray() {
+    }
+
+    @Test
+    public void copyArray() {
+    }
+
+    @Test
+    public void isEqual() {
+    }
+
+    @Test
+    public void printArray() {
+    }
+
+
+    /** 冒泡排序，两两交换，如果a[i]>a[i+1],a[i]放在后面
+     * 1，如果数组为空，或者数组的值小于2，直接输出（一个数不用排）
+     * 2，
+     * @param arr
+     */
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int end = arr.length - 1; end > 0; end--) { // 比较趟数 ：一开始是end,然后end-1
+            for (int i = 0; i < end; i++) { // 数组长度
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * 两数交换
+     * @param arr
+     * @param i
+     * @param j
+     */
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
---
+++ ZuoChengYun/src/test/java/com/nateshao/basic_class_01/Code_01_InsertionSortTest.java
@@ -0,0 +1,79 @@
+package com.nateshao.basic_class_01;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * @date Created by 邵桐杰 on 2021/9/11 17:17
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 选择排序，比较次数，哪个数字小，就放在前面
+ */
+public class Code_01_InsertionSortTest {
+
+    @Test
+    public void selectionSortTest() {
+        int[] arr = {1, 4, 2, 3, 5, 6, 3, 9, 7, 8};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * 选择排序，小的放在后面
+     * 1，如果数组为空，或者数组的值小于2，直接输出（一个数不用排）
+     * 2，
+     * @param arr
+     */
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minIndex = arr[j] < arr[minIndex] ? j : minIndex;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    @Test
+    public void comparator() {
+    }
+
+    @Test
+    public void generateRandomArray() {
+    }
+
+    @Test
+    public void copyArray() {
+    }
+
+    @Test
+    public void isEqual() {
+    }
+
+    @Test
+    public void printArray() {
+    }
+
+    @Test
+    public void main() {
+        int[] arr ={1,4,2,5,7,28,9};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
