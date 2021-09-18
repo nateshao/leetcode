1d6b36abce1e2803217535c674b9ffe75afeb125
nateshao <shaotongjie@gmail.com>
2021-09-18 16:51:19 +0800

    bootstrap技术教程

--- ZuoChengYun/src/main/java/com/nateshao/basic_class_01/Code_08_NetherlandsFlag.java
+++ ZuoChengYun/src/main/java/com/nateshao/basic_class_01/Code_08_NetherlandsFlag.java
@@ -14,57 +14,57 @@ package com.nateshao.basic_class_01;
  */
 public class Code_08_NetherlandsFlag {
 
-	public static int[] partition(int[] arr, int l, int r, int num) {
-		int less = l - 1;
-		int more = r + 1;
-		int cur = l;
-		while (cur < more) {
-			if (arr[cur] < num) {
-				swap(arr, ++less, cur++);
-			} else if (arr[cur] > num) {
-				swap(arr, --more, cur);
-			} else {
-				cur++;  // cur == num
-			}
-		}
-		return new int[] { less + 1, more - 1 };
-	}
+    public static int[] partition(int[] arr, int l, int r, int num) {
+        int less = l - 1;
+        int more = r + 1;
+        int cur = l;
+        while (cur < more) {
+            if (arr[cur] < num) {
+                swap(arr, ++less, cur++);
+            } else if (arr[cur] > num) {
+                swap(arr, --more, cur);
+            } else {
+                cur++;  // cur == num
+            }
+        }
+        return new int[]{less + 1, more - 1};
+    }
 
-	// for test
-	public static void swap(int[] arr, int i, int j) {
-		int tmp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tmp;
-	}
+    // for test
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 
-	// for test
-	public static int[] generateArray() {
-		int[] arr = new int[10];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) (Math.random() * 3);
-		}
-		return arr;
-	}
+    // for test
+    public static int[] generateArray() {
+        int[] arr = new int[10];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 3);
+        }
+        return arr;
+    }
 
-	// for test
-	public static void printArray(int[] arr) {
-		if (arr == null) {
-			return;
-		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 
-	public static void main(String[] args) {
-		int[] test = generateArray();
+    public static void main(String[] args) {
+        int[] test = generateArray();
 
-		printArray(test);
-		int[] res = partition(test, 0, test.length - 1, 1);
-		printArray(test);
-		System.out.println(res[0]);
-		System.out.println(res[1]);
+        printArray(test);
+        int[] res = partition(test, 0, test.length - 1, 1);
+        printArray(test);
+        System.out.println(res[0]);
+        System.out.println(res[1]);
 
-	}
+    }
 }
