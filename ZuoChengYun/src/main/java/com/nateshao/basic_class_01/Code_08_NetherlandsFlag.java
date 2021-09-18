4b6d759ebe18170e19e01566522fa50f5427ce2a
nateshao <shaotongjie@gmail.com>
2021-09-18 16:42:00 +0800

    代码整理

--- ZuoChengYun/src/main/java/com/nateshao/basic_class_01/Code_08_NetherlandsFlag.java
+++ ZuoChengYun/src/main/java/com/nateshao/basic_class_01/Code_08_NetherlandsFlag.java
@@ -1,17 +1,30 @@
 package com.nateshao.basic_class_01;
 
+/**
+ * @date Created by 邵桐杰 on 2021/9/18 16:35
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 问题二（荷兰国旗问题）
+ * 给定一个数组arr，和一个数num，请把小于num的数放在数组的左边，等于num的数放在数组的中间，大于num的数放在数组的右边。
+ * 要求额外空间复杂度O(1)，时间复杂度O(N)
+ */
 public class Code_08_NetherlandsFlag {
 
-	public static int[] partition(int[] arr, int l, int r, int p) {
+	public static int[] partition(int[] arr, int l, int r, int num) {
 		int less = l - 1;
 		int more = r + 1;
-		while (l < more) {
-			if (arr[l] < p) {
-				swap(arr, ++less, l++);
-			} else if (arr[l] > p) {
-				swap(arr, --more, l);
+		int cur = l;
+		while (cur < more) {
+			if (arr[cur] < num) {
+				swap(arr, ++less, cur++);
+			} else if (arr[cur] > num) {
+				swap(arr, --more, cur);
 			} else {
-				l++;
+				cur++;  // cur == num
 			}
 		}
 		return new int[] { less + 1, more - 1 };
