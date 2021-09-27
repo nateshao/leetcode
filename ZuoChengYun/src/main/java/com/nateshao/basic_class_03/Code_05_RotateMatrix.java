6c431b3602a5a6da5a4bdf1d2eff0708f4c63cc7
nateshao <shaotongjie@gmail.com>
2021-09-27 16:41:27 +0800

    代码整理

--- ZuoChengYun/src/main/java/com/nateshao/basic_class_03/Code_05_RotateMatrix.java
+++ ZuoChengYun/src/main/java/com/nateshao/basic_class_03/Code_05_RotateMatrix.java
@@ -1,5 +1,16 @@
 package com.nateshao.basic_class_03;
 
+/**
+ * @date Created by 邵桐杰 on 2021/9/18 20:03
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:旋转正方形矩阵
+ * 【题目】 给定一个整型正方形矩阵matrix，请把该矩阵调整成
+ * 顺时针旋转90度的样子。
+ */
 public class Code_05_RotateMatrix {
     public static void main(String[] args) {
         int[][] matrix = {{1, 2, 3, 4},
@@ -23,11 +34,21 @@ public class Code_05_RotateMatrix {
         }
     }
 
+    /**
+     *
+     * @param m
+     * @param tR    左上角的行
+     * @param tC    左上角的列
+     * @param dR    右上角的行
+     * @param dC    右上角的列
+     *              正方形，四角角顺时针交换位置，然后下一位也是四个数交换
+     */
     public static void rotateEdge(int[][] m, int tR, int tC, int dR, int dC) {
         int times = dC - tC;
         int tmp = 0;
         for (int i = 0; i != times; i++) {
             tmp = m[tR][tC + i];
+            // 四个点交换 （旋转90度）
             m[tR][tC + i] = m[dR - i][tC];
             m[dR - i][tC] = m[dR][dC - i];
             m[dR][dC - i] = m[tR + i][dC];
@@ -43,6 +64,4 @@ public class Code_05_RotateMatrix {
             System.out.println();
         }
     }
-
-
 }
