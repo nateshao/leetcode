5a6af52176d09337c2ad0fb3d7bdf48e2987f6b9
nateshao <shaotongjie@gmail.com>
2021-10-09 22:19:03 +0800

    代码整理

--- ZuoChengYun/src/main/java/com/nateshao/basic_class_03/Code_13_CopyListWithRandom.java
+++ ZuoChengYun/src/main/java/com/nateshao/basic_class_03/Code_13_CopyListWithRandom.java
@@ -2,7 +2,55 @@ package com.nateshao.basic_class_03;
 
 import java.util.HashMap;
 
+/**
+ * 复制含有随机指针节点的链表
+ * [题目]一 种特殊的链表节点类描述如下:
+ * public class Node { public int value; public Node next; public Node rand;
+ * public Node(int data){ this. value = data; }
+ * Node类中的value是节点值，next指针和正常单链表中next指针的意义
+ * 一样，都指向下一个节点，rand指针是Node类中新增的指针，这个指
+ * 针可能指向链表中的任意一个节点，也可能指向null。给定- -个由
+ * Node节点类型组成的无环单链表的头节点head,请实现一个函数完成
+ * 这个链表中所有结构的复制，并返回复制的新链表的头节点。进阶 :
+ * 不使用额外的数据结构，只用有限几个变量，且在时间复杂度为0(N)
+ * 内完成原问题要实现的函数。
+ */
 public class Code_13_CopyListWithRandom {
+	public static void main(String[] args) {
+		Node head = null;
+		Node res1 = null;
+		Node res2 = null;
+		printRandLinkedList(head);
+		res1 = copyListWithRand1(head);
+		printRandLinkedList(res1);
+		res2 = copyListWithRand2(head);
+		printRandLinkedList(res2);
+		printRandLinkedList(head);
+		System.out.println("=========================");
+
+		head = new Node(1);
+		head.next = new Node(2);
+		head.next.next = new Node(3);
+		head.next.next.next = new Node(4);
+		head.next.next.next.next = new Node(5);
+		head.next.next.next.next.next = new Node(6);
+
+		head.rand = head.next.next.next.next.next; // 1 -> 6
+		head.next.rand = head.next.next.next.next.next; // 2 -> 6
+		head.next.next.rand = head.next.next.next.next; // 3 -> 5
+		head.next.next.next.rand = head.next.next; // 4 -> 3
+		head.next.next.next.next.rand = null; // 5 -> null
+		head.next.next.next.next.next.rand = head.next.next.next; // 6 -> 4
+
+		printRandLinkedList(head);
+		res1 = copyListWithRand1(head);
+		printRandLinkedList(res1);
+		res2 = copyListWithRand2(head);
+		printRandLinkedList(res2);
+		printRandLinkedList(head);
+		System.out.println("=========================");
+
+	}
 
 	public static class Node {
 		public int value;
@@ -82,40 +130,5 @@ public class Code_13_CopyListWithRandom {
 		System.out.println();
 	}
 
-	public static void main(String[] args) {
-		Node head = null;
-		Node res1 = null;
-		Node res2 = null;
-		printRandLinkedList(head);
-		res1 = copyListWithRand1(head);
-		printRandLinkedList(res1);
-		res2 = copyListWithRand2(head);
-		printRandLinkedList(res2);
-		printRandLinkedList(head);
-		System.out.println("=========================");
-
-		head = new Node(1);
-		head.next = new Node(2);
-		head.next.next = new Node(3);
-		head.next.next.next = new Node(4);
-		head.next.next.next.next = new Node(5);
-		head.next.next.next.next.next = new Node(6);
-
-		head.rand = head.next.next.next.next.next; // 1 -> 6
-		head.next.rand = head.next.next.next.next.next; // 2 -> 6
-		head.next.next.rand = head.next.next.next.next; // 3 -> 5
-		head.next.next.next.rand = head.next.next; // 4 -> 3
-		head.next.next.next.next.rand = null; // 5 -> null
-		head.next.next.next.next.next.rand = head.next.next.next; // 6 -> 4
-
-		printRandLinkedList(head);
-		res1 = copyListWithRand1(head);
-		printRandLinkedList(res1);
-		res2 = copyListWithRand2(head);
-		printRandLinkedList(res2);
-		printRandLinkedList(head);
-		System.out.println("=========================");
-
-	}
 
 }
