5d6d388e67d596481dac8318520705845f00b0b6
nateshao <shaotongjie@gmail.com>
2021-10-29 16:04:01 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/basic_01_ten_sort/Code_01_BubbleSort.java
+++ algo-notes/src/main/java/com/nateshao/basic_01_ten_sort/Code_01_BubbleSort.java
@@ -7,8 +7,94 @@ package com.nateshao.basic_01_ten_sort;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 冒泡排序
  */
 public class Code_01_BubbleSort {
+
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 500000;
+        int maxValue = 500000;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+
+        }
+    }
+
+    /**
+     * 生成任意数组
+     *
+     * @param maxSize  大小
+     * @param maxValue 最大值
+     * @return
+     */
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) ((maxValue) * Math.random());
+        }
+        return arr;
+    }
+
+    /**
+     * 数组复制
+     *
+     * @param arr 原数组
+     * @return 如果数组为null，则返回null；否则创建一个新的数组，遍历原数组的长度，依次赋值。
+     */
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    /**
+     * 判断两个数组是否相等
+     *
+     * @param arr1
+     * @param arr2
+     * @return
+     */
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        // 条件1：数组arr1为空and数组arr2不为空 or 数组arr1不为空and数组arr2为空 。return false
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        // 条件2：两个是数组都为空的情况下。
+        if (arr1 == null && arr2 == null) {
+            return false;
+        }
+        // 条件3：两数组长度不相等。
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        // 条件4：两数组内元素不相等。
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 打印数组
+     * @param arr
+     */
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
 
 }
