748ada6d41fe54a15646835926b6ea1b441da59d
nateshao <shaotongjie@gmail.com>
2021-11-09 23:15:30 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_00/TreeNode.java
@@ -0,0 +1,20 @@
+package com.nateshao.sword_offer.topic_00;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/9 23:13
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 定义二叉树
+ */
+public class TreeNode {
+    int val;
+    TreeNode left; // 左孩子
+    TreeNode right; // 右孩子
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
