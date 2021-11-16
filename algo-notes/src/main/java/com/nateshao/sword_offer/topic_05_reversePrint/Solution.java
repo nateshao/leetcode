2df09aebc4da0ab67e11389d14671cf571f123ac
nateshao <shaotongjie@gmail.com>
2021-11-16 23:13:11 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_05_reversePrint/Solution.java
@@ -0,0 +1,20 @@
+package com.nateshao.sword_offer.topic_05_reversePrint;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/16 23:12
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 剑指 Offer 05. 从尾到头打印链表
+ * > 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * >
+ * > 示例 1：
+ * > 输入：head = [1,3,2]
+ * > 输出：[2,3,1]
+ * > 限制：0 <= 链表长度 <= 10000
+ */
+public class Solution {
+
+}
