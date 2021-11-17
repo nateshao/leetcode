04fbec03bb9f5883c804101fa5e1a742d9485aaf
nateshao <shaotongjie@gmail.com>
2021-11-17 23:25:01 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_06_buildTree/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_06_buildTree/Solution.java
@@ -1,5 +1,8 @@
 package com.nateshao.sword_offer.topic_06_buildTree;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * @date Created by 邵桐杰 on 2021/11/17 10:48
  * @微信公众号 程序员千羽
@@ -10,10 +13,70 @@ package com.nateshao.sword_offer.topic_06_buildTree;
  * Description:
  */
 public class Solution {
+    public static void main(String[] args) {
+        int[] preorder = {1, 2, 4, 7, 3, 5, 6, 8};
+        int[] inorder = {4, 7, 2, 1, 5, 3, 8, 6};
 
+
+    }
+
+    HashMap<Integer, Integer> map = new HashMap<>();//标记中序遍历
+    int[] preorder;//保留的先序遍历，方便递归时依据索引查看先序遍历的值
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        //将中序遍历的值及索引放在map中，方便递归时获取左子树与右子树的数量及其根的索引
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        //三个索引分别为
+        //当前根的的索引
+        //递归树的左边界，即数组左边界
+        //递归树的右边界，即数组右边界
+        return recur(0, 0, inorder.length - 1);
+    }
 
+    TreeNode recur(int pre_root, int in_left, int in_right) {
+        if (in_left > in_right) return null;// 相等的话就是自己
+        TreeNode root = new TreeNode(preorder[pre_root]);//获取root节点
+        int idx = map.get(preorder[pre_root]);//获取在中序遍历中根节点所在索引，以方便获取左子树的数量
+        //左子树的根的索引为先序中的根节点+1
+        //递归左子树的左边界为原来的中序in_left
+        //递归左子树的右边界为中序中的根节点索引-1
+        root.left = recur(pre_root + 1, in_left, idx - 1);
+        //右子树的根的索引为先序中的 当前根位置 + 左子树的数量 + 1
+        //递归右子树的左边界为中序中当前根节点+1
+        //递归右子树的右边界为中序中原来右子树的边界
+        root.right = recur(pre_root + (idx - in_left) + 1, idx + 1, in_right);
+        return root;
+    }
+
+    /**
+     * 思路：先找出根节点，然后利用递归方法构造二叉树
+     * 代码实现：时间复杂度：O(n)，空间复杂度：O(n)
+     * 解法一：递归（传入数组的拷贝）
+     *
+     * @param pre
+     * @param in
+     * @return
+     */
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        if (pre == null || in == null || pre.length == 0 || in.length == 0) {
+            return null;
+        }
+        if (pre.length != in.length) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pre[0]);
+        for (int i = 0; i < pre.length; i++) {
+            if (pre[0] == in[i]) {
+                root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1,
+                        i + 1), Arrays.copyOfRange(in, 0, i));
+                root.right = reConstructBinaryTree(Arrays.copyOfRange(pre,
+                        i + 1, pre.length), Arrays.copyOfRange(in, i + 1, in.length));
+            }
+        }
+        return root;
     }
 
     public class TreeNode {
