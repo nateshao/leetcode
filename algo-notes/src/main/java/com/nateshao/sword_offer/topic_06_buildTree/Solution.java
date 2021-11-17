b0af21f95a2a059ede2172bee751f5a7e4ebb72e
nateshao <shaotongjie@gmail.com>
2021-11-17 10:49:30 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_06_buildTree/Solution.java
@@ -0,0 +1,28 @@
+package com.nateshao.sword_offer.topic_06_buildTree;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/17 10:48
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Solution {
+
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
