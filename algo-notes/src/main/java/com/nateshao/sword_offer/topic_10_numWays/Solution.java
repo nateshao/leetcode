c448f5c6ccb91cdbc3d64cf3a8dd28a533cc3082
nateshao <shaotongjie@gmail.com>
2021-11-19 23:40:49 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_10_numWays/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_10_numWays/Solution.java
@@ -7,9 +7,45 @@ package com.nateshao.sword_offer.topic_10_numWays;
  * @博客 https://nateshao.gitee.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 青蛙跳台阶问题
  */
 public class Solution {
+    public static void main(String[] args) {
+        int i = numWays(7);
+        System.out.println("i = " + i);
+        int i1 = JumpFloor(7);
+        System.out.println("i1 = " + i1);
+    }
 
+    public static int numWays(int n) {
+        int a = 1, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+
+    /**
+     * 思路：斐波那契数列思想
+     *
+     * @param n
+     * @return
+     */
+    public static int JumpFloor(int n) {
+        if (n < 3) {
+            return n;
+        }
+        int result = 0;
+        int preOne = 2;
+        int preTwo = 1;
+        for (int i = 3; i <= n; i++) {
+            result = preOne + preTwo;
+            preTwo = preOne;
+            preOne = result;
+        }
+        return result;
+    }
 
 }
