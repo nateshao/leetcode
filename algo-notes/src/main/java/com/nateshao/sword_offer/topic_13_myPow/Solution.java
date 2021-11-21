47996cb7405925af5eacab875b829123d9c6b18d
nateshao <shaotongjie@gmail.com>
2021-11-21 10:49:01 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_13_myPow/Solution.java
@@ -0,0 +1,18 @@
+package com.nateshao.sword_offer.topic_13_myPow;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/21 10:48
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:  剑指 Offer 16. 数值的整数次方
+ * 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。不得使用库函数，同时不需要考虑大数问题。
+ */
+public class Solution {
+
+    public double myPow(double x, int n) {
+
+    }
+}
