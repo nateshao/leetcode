2a880b12d809020c576ef997a1c112f952f06cb4
nateshao <shaotongjie@gmail.com>
2021-11-21 14:09:46 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_13_myPow/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_13_myPow/Solution.java
@@ -11,8 +11,35 @@ package com.nateshao.sword_offer.topic_13_myPow;
  * 实现 pow(x, n) ，即计算 x 的 n 次幂函数（即，xn）。不得使用库函数，同时不需要考虑大数问题。
  */
 public class Solution {
+    public static void main(String[] args) {
+        double v = myPow1(2, 3);
+        double v2 = myPow1(2, 3);
+        System.out.println("v = " + v);
+        System.out.println("v2 = " + v2);
+    }
 
-    public double myPow(double x, int n) {
+    public static double myPow1(double x, int n) {
+        if (n == 0 || x == 1.0) return 1.0;
+        if (n == 1) return x;
+        if (n == -1) return 1.0 / x;
 
+        double res = myPow1(x, n / 2);
+        res = res * res;
+
+        //如果是奇数
+        if ((n & 1) == 1 && n > 0) res = res * x; // 就是判断奇偶，=1为奇数，比%效率更高
+        if ((n & 1) == 1 && n < 0) res = res * 1 / x;
+
+        return res;
+
+    }
+
+    public double myPow2(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+        if (n == -1) return 1 / x;
+        double half = myPow2(x, n >> 1); // n>>1就是n/2，但是右移效率更高
+        double rest = myPow2(x, n & 1);
+        return half * half * rest;
     }
 }
