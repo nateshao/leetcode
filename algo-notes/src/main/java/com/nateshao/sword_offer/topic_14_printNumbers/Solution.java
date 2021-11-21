a9e446659029a344dd4611330aeee84d827dc810
nateshao <shaotongjie@gmail.com>
2021-11-21 16:35:45 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_14_printNumbers/Solution.java
@@ -0,0 +1,32 @@
+package com.nateshao.sword_offer.topic_14_printNumbers;
+
+import java.util.Arrays;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/21 16:22
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] ints = printNumbers(3);
+        for (int anInt : ints) {
+            System.out.print(anInt + " ");
+        }
+        Arrays.toString(ints);
+    }
+
+    public static int[] printNumbers(int n) {
+        int end = (int) Math.pow(10, n) - 1;
+        int[] res = new int[end];
+        for (int i = 0; i < end; i++)
+            res[i] = i + 1;
+        return res;
+    }
+
+}
