840484088bac06ca2760a9a8dad16741793a9c8a
nateshao <shaotongjie@gmail.com>
2021-11-21 22:20:55 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_15_deleteNode/Solution.java
@@ -0,0 +1,91 @@
+package com.nateshao.sword_offer.topic_15_deleteNode;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/21 16:22
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 删除链表的节点
+ */
+public class Solution {
+
+
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head.val == val) return head.next;
+        ListNode pre = head, cur = head.next;
+        while (cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if (cur != null) pre.next = cur.next;
+        return head;
+    }
+
+    public void deleteNode(ListNode head, ListNode deListNode) {
+        if (deListNode == null || head == null)
+            return;
+        if (head == deListNode) {
+            head = null;
+        } else {
+            // 若删除节点是末尾节点，往后移一个
+            if (deListNode.next == null) {
+                ListNode pointListNode = head;
+                while (pointListNode.next.next != null) {
+                    pointListNode = pointListNode.next;
+                }
+                pointListNode.next = null;
+            } else {
+                deListNode.val = deListNode.next.val;
+                deListNode.next = deListNode.next.next;
+            }
+        }
+    }
+
+    /**
+     * 单指针实现
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode deleteNode2(ListNode head, int val) {
+        if (head == null) return null;
+        if (head.val == val) return head.next;
+        ListNode cur = head;
+        while (cur.next != null && cur.next.val != val) {
+            cur = cur.next;
+        }
+        if (cur.next != null) {
+            cur.next = cur.next.next;
+        }
+        return head;
+    }
+
+    /**
+     * 递归实现
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode deleteNode3(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        if (head.val == val) {
+            return head.next;
+        } else {
+            head.next = deleteNode3(head.next, val);
+        }
+        return head;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
