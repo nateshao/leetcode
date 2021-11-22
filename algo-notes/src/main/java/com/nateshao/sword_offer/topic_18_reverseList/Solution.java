b844081a47e3ac72bfb95c3632a3dc9140ab6869
nateshao <shaotongjie@gmail.com>
2021-11-22 22:30:51 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_18_reverseList/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_18_reverseList/Solution.java
@@ -10,6 +10,7 @@ package com.nateshao.sword_offer.topic_18_reverseList;
  * Description: 反转链表
  * 思路：定义两个指针，反向输出
  * https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof
+ * https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/solution/jian-zhi-offer-24-fan-zhuan-lian-biao-die-dai-di-2/
  */
 public class Solution {
 
@@ -33,16 +34,48 @@ public class Solution {
     }
 
     /**
-     *解法二：递归，时间复杂度：O(n)，空间复杂度：O(n)
+     * 解法一：迭代2：两个指针
+     * 精选解答
+     *
      * @param head
      * @return
      */
-    public ListNode reverseList2(ListNode head) {
-        if (head == null || head.next == null) return null;
-        ListNode p = reverseList2(head.next);
-        head.next.next = head.next;
+    public ListNode reverseList1(ListNode head) {
+        ListNode cur = head, pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
+        }
+        return pre;
+    }
+
+    /**
+     * 递归：时间复杂度：O(n)，空间复杂度：O(n)
+     *
+     * @param head
+     * @return
+     */
+    public static ListNode reverseList3(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList3(head.next);
+        head.next.next = head;
         head.next = null;
-        return p;
+        return newHead;
+    }
+
+    public ListNode reverseList4(ListNode head) {
+        ListNode cur = head, pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next; // 暂存后继节点 cur.next
+            cur.next = pre;          // 修改 next 引用指向
+            pre = cur;               // pre 暂存 cur
+            cur = tmp;               // cur 访问下一节点
+        }
+        return pre;
     }
 
 
@@ -54,4 +87,5 @@ public class Solution {
             val = x;
         }
     }
+
 }
