af1674f0f156f4f26c19a4bfda9d06a00522ef83
nateshao <shaotongjie@gmail.com>
2021-11-28 21:53:43 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_24_validateStackSequences/Solution.java
@@ -0,0 +1,16 @@
+package com.nateshao.sword_offer.topic_24_validateStackSequences;
+
+/**
+ * @date Created by 邵桐杰 on 2021/11/28 21:53
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+    }
+}
