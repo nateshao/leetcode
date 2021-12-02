703ce61cd8c1e0bc2d1aae0da895cfeed88de98a
nateshao <shaotongjie@gmail.com>
2021-12-02 15:43:39 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_29_treeToDoublyList/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_29_treeToDoublyList/Solution.java
@@ -10,4 +10,30 @@ package com.nateshao.sword_offer.topic_29_treeToDoublyList;
  * Description: 二叉搜索树与双向链表
  */
 public class Solution {
+
+    public Node treeToDoublyList(Node root) {
+
+
+    }
+
+
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val,Node _left,Node _right) {
+            val = _val;
+            left = _left;
+            right = _right;
+        }
+    };
+
 }
