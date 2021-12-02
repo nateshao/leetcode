9124d43cdc30e093e2cb63aaa69550158d45edaf
nateshao <shaotongjie@gmail.com>
2021-12-02 14:07:10 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_27_pathSum/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_27_pathSum/Solution.java
@@ -101,7 +101,6 @@ public class Solution {
         TreeNode(int val) {
             this.val = val;
         }
-
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_28_copyRandomList/Solution.java
@@ -0,0 +1,32 @@
+package com.nateshao.sword_offer.topic_28_copyRandomList;
+
+/**
+ * @date Created by 邵桐杰 on 2021/12/2 14:05
+ * @微信公众号 程序员千羽
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:  复杂链表的复制
+ */
+public class Solution {
+
+
+
+    public Node copyRandomList(Node head) {
+
+    }
+
+    // Definition for a Node.
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+}
