87c4cbcb0f6b6eddc5e42b1b0881a1ec74c4605b
nateshao <shaotongjie@gmail.com>
2022-01-17 22:37:19 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_39_getIntersectionNode/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_39_getIntersectionNode/Solution.java
@@ -1,5 +1,8 @@
 package com.nateshao.sword_offer.topic_39_getIntersectionNode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @date Created by 邵桐杰 on 2022/1/1 23:45
  * @微信公众号 千羽的编程时光
@@ -10,16 +13,44 @@ package com.nateshao.sword_offer.topic_39_getIntersectionNode;
  * Description:
  */
 public class Solution {
-
+    /**
+     * 双指针
+     * @param headA
+     * @param headB
+     * @return
+     */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null||headB==null) return null;
         ListNode A = headA, B = headB;
         while (A != B) {
             A = A != null ? A.next : headB;
             B = B != null ? B.next : headA;
         }
         return A;
-
     }
+
+    /**
+     * 哈希集合
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
+        Set<ListNode> visited = new HashSet<>();
+        ListNode temp = headA;
+        while (temp!=null){
+            visited.add(temp);
+            temp = temp.next;
+        }
+
+        temp = headB;
+        while (headB!=null){
+            if (visited.contains(temp))return temp;
+            temp=temp.next;
+        }
+        return null;
+    }
+
 
     public class ListNode {
         int val;
