3e719c0e0a7a50156122e6e1f336575f015a8404
nateshao <shaotongjie@gmail.com>
2022-01-19 22:18:31 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/one_question_per_day/code_2022/january/Solution_2022_01_19_containsNearbyDuplicate.java
@@ -0,0 +1,28 @@
+package com.nateshao.one_question_per_day.code_2022.january;
+
+import java.util.HashSet;
+
+/**
+ * @date Created by 邵桐杰 on 2022/1/19 22:03
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode-cn.com/problems/contains-duplicate-ii/solution/hua-jie-suan-fa-219-cun-zai-zhong-fu-yuan-su-ii-by/
+ */
+public class Solution_2022_01_19_containsNearbyDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])){
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size()>k){
+                set.remove(nums[i-k]);
+            }
+        }
+        return false;
+    }
+}
