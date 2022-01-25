860aff0701977651c942b984e8860d367ae4edcd
nateshao <shaotongjie@gmail.com>
2022-01-25 23:12:58 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_44_findContinuousSequence/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_44_findContinuousSequence/Solution.java
@@ -14,12 +14,38 @@ import java.util.List;
  */
 public class Solution {
     public static void main(String[] args) {
-
+        int[][] findContinuousSequence = findContinuousSequence(9);
+        for (int[] ints : findContinuousSequence) {
+            for (int anInt : ints) {
+                System.out.println("anInt = " + anInt);
+            }
+        }
     }
 
-    public int[][] findContinuousSequence(int target) {
+    /**
+     * 解法：滑动窗口（双指针）
+     * @param target
+     * @return
+     */
+    public static int[][] findContinuousSequence(int target) {
         int i = 1, j = 2, s = 3;
-
+        List<int[]> res = new ArrayList<>();
+        while (i < j) {
+            if (s == target) {
+                int[] ans = new int[j - i + 1];
+                for (int k = i; k <= j; k++)
+                    ans[k - i] = k;
+                res.add(ans);
+            }
+            if (s >= target) {
+                s -= i;
+                i++;
+            } else {
+                j++;
+                s += j;
+            }
+        }
+        return res.toArray(new int[0][]);
     }
 
 }
