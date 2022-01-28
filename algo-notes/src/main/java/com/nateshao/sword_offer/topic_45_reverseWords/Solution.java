f6d0d7b0bb1a41269bd158b7ae1f01706338f01c
nateshao <shaotongjie@gmail.com>
2022-01-28 23:13:33 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_45_reverseWords/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_45_reverseWords/Solution.java
@@ -14,13 +14,36 @@ import javax.swing.*;
 public class Solution {
     public static void main(String[] args) {
         String s = "the sky is blue";
+        System.out.println("reverseWords1(s) = " + reverseWords1(s));
 //        System.out.println("reverseSentence(s) = " + reverseSentence(s));//reverseSentence(s) = blue is sky the
-        System.out.println("reverseWords2(s) = " + reverseWords2(s));
+//        System.out.println("reverseWords2(s) = " + reverseWords2(s));
     }
 
-    //    public String reverseWords(String s) {
-//
-//    }
+    /**
+     * 双指针
+     * 1. 倒序遍历字符串，记录单词左右索引边界i,j
+     * 2. 每确定单词的边界，放入res
+     * 3. 最后，拼接成字符串，返回即可
+     *
+     * @param s
+     * @return
+     */
+    public static String reverseWords1(String s) {
+        s = s.trim();
+        int j = s.length() - 1, i = j;
+        StringBuilder res = new StringBuilder();
+        while (i >= 0) {
+            // 搜索首个空格，添加单词
+            // 是字母就添加进去
+            while (i >= 0 && s.charAt(i) != ' ') i--;
+            res.append(s.substring(i + 1, j + 1) + " ");
+            // 是空格的话，跳过单词间空格，j就指向下一个单词尾字符。
+            while (i >= 0 && s.charAt(i) == ' ') i--;
+            j = i;
+        }
+        return res.toString().trim();// 转化为字符串，并且返回
+    }
+
     // 方法二：分割 + 倒序 （面试时不建议使用）
     public static String reverseWords2(String s) {
         // 删除首尾空格，分割字符串
