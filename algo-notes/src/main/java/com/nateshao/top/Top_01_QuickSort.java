1dc1c508b502b28428acc26fe5922f46b49e5987
nateshao <shaotongjie@gmail.com>
2022-02-23 23:21:12 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/top/Top_01_QuickSort.java
@@ -0,0 +1,64 @@
+package com.nateshao.top;
+
+import java.util.Arrays;
+
+/**
+ * @date Created by 邵桐杰 on 2022/2/23 21:50
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: Java实现递归与非递归的快速排序
+ */
+public class Top_01_QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * @param arr        待排序列
+     * @param leftIndex  待排序列起始位置
+     * @param rightIndex 待排序列结束位置
+     */
+    private static void quickSort(int[] arr, int leftIndex, int rightIndex) {
+        /**
+         * 如果左边大于右边，直接返回
+         */
+        if (leftIndex >= rightIndex) return;
+
+        int left = leftIndex;
+        int right = rightIndex;
+        //待排序的第一个元素作为基准值
+        int key = arr[left];
+
+        //从左右两边交替扫描，直到left = right
+        while (left < right) {
+            while (right > left && arr[right] >= key) {
+                //从右往左扫描，找到第一个比基准值小的元素
+                right--;
+            }
+
+            //找到这种元素将arr[right]放入arr[left]中
+            arr[left] = arr[right];
+
+            while (left < right && arr[left] <= key) {
+                //从左往右扫描，找到第一个比基准值大的元素
+                left++;
+            }
+
+            //找到这种元素将arr[left]放入arr[right]中
+            arr[right] = arr[left];
+        }
+        //基准值归位
+        arr[left] = key;
+        //对基准值左边的元素进行递归排序
+        quickSort(arr, leftIndex, left - 1);
+        //对基准值右边的元素进行递归排序。
+        quickSort(arr, right + 1, rightIndex);
+    }
+
+}
