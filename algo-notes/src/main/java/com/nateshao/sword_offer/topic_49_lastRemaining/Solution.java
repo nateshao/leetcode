d2a1365f54748f4deb86fa8c8a4e6509865759af
nateshao <shaotongjie@gmail.com>
2022-02-24 22:35:18 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_49_lastRemaining/Solution.java
@@ -0,0 +1,46 @@
+package com.nateshao.sword_offer.topic_49_lastRemaining;
+
+/**
+ * @date Created by 邵桐杰 on 2022/2/24 21:51
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+ * <p>
+ * 剑指 Offer 62. 圆圈中最后剩下的数字
+ * 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。求出这个圆圈里剩下的最后一个数字。
+ * <p>
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: n = 5, m = 3
+ * 输出: 3
+ * 示例 2：
+ * <p>
+ * 输入: n = 10, m = 17
+ * 输出: 2
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int i = lastRemaining(5, 3);
+        System.out.println("i = " + i);
+    }
+
+    /**
+     * 利用公式法：f[n] = (f[n-1] + k) mod n，或使用循环链表实现
+     * @param n
+     * @param m
+     * @return
+     */
+    public static int lastRemaining(int n, int m) {
+        int x = 0;
+        for (int i = 2; i <= n; i++) {
+            // i 个人时删除数的索引等于 i-1 个人时删除数的索引+k(再对i 取余)
+            x = (x + m) % i;
+        }
+        return x;
+    }
+}
---
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_50_lsumNums/Solution.java
@@ -0,0 +1,28 @@
+package com.nateshao.sword_offer.topic_50_lsumNums;
+
+/**
+ * @date Created by 邵桐杰 on 2022/2/24 22:34
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode-cn.com/problems/qiu-12n-lcof/
+ * 剑指 Offer 64. 求1+2+…+n
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+ *
+ * 示例 1：
+ *
+ * 输入: n = 3
+ * 输出: 6
+ * 示例 2：
+ *
+ * 输入: n = 9
+ * 输出: 45
+ */
+public class Solution {
+
+    public int sumNums(int n) {
+
+    }
+}
