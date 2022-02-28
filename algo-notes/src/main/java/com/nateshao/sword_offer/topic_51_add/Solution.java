54c9594be3daa1b2236c2e4adf6f06f27caa3ed8
nateshao <shaotongjie@gmail.com>
2022-02-28 22:58:04 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer/topic_51_add/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer/topic_51_add/Solution.java
@@ -15,11 +15,24 @@ package com.nateshao.sword_offer.topic_51_add;
  * <p>
  * 输入: a = 1, b = 1
  * 输出: 2
+ *
+ * 复杂度分析：
+ * 时间复杂度 O(1) ： 最差情况下（例如 a = 0x7fffffff , b = 1 时），需循环 32 次，使用 O(1)时间；每轮中的常数次位操作使用 O(1) 时间。
+ * 空间复杂度 O(1)： 使用常数大小的额外空间。
+ *
+ *
  */
 public class Solution {
-    public int add(int a, int b) {
-        while (b != 0) {
+    public static void main(String[] args) {
+        System.out.println("add(1, 2) = " + add(1, 2));
+    }
 
+    public static int add(int a, int b) {
+        while (b != 0) {
+            int sum = a ^ b;// 计算个位
+            b = (a & b) << 1; // 计算进位
+            a = sum;
         }
+        return a;
     }
 }
