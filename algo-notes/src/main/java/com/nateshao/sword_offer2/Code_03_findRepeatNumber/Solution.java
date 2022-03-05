777b2791e9fb653bb64b965a08017f2ee9b29a3c
nateshao <shaotongjie@gmail.com>
2022-03-05 21:33:19 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_03_findRepeatNumber/Solution.java
@@ -0,0 +1,52 @@
+package com.nateshao.sword_offer2.Code_03_findRepeatNumber;
+
+import java.util.HashSet;
+
+/**
+ * @date Created by 邵桐杰 on 2022/3/5 21:16
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 剑指 Offer 03. 数组中重复的数字
+ * <p>
+ * 找出数组中重复的数字。
+ * <p>
+ * <p>
+ * 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：
+ * [2, 3, 1, 0, 2, 5, 3]
+ * 输出：2 或 3
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 1, 0, 2, 5, 3};
+        int number = findRepeatNumber(nums);
+        System.out.println("number = " + number);
+    }
+    /**
+     * 用hashset
+     * 遍历数组，如果数组中有相同的话，说明重复直接返回。
+     * 将不重复的添加到hashset
+     *
+     * @param nums
+     * @return
+     */
+    public static int findRepeatNumber(int[] nums) {
+        HashSet<Object> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return num;
+            set.add(num);
+        }
+        return -1;
+    }
+
+}
---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_04_findNumberIn2DArray/Solution.java
@@ -0,0 +1,66 @@
+package com.nateshao.sword_offer2.Code_04_findNumberIn2DArray;
+
+/**
+ * @date Created by 邵桐杰 on 2022/3/5 21:23
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 04. 二维数组中的查找
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个高效的函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ * 示例:
+ * <p>
+ * 现有矩阵 matrix 如下：
+ * <p>
+ * [
+ * [1,   4,  7, 11, 15],
+ * [2,   5,  8, 12, 19],
+ * [3,   6,  9, 16, 22],
+ * [10, 13, 14, 17, 24],
+ * [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ * <p>
+ * 给定 target = 20，返回 false。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    /**
+     * 个人理解，也就是说从（i,j）左下脚开始找，
+     * 如果比目标值小，向上找，i--
+     * 如果比目标值大，向右找，j++
+     * 最后找到了，return true
+     * 否则 return false
+     * <p>
+     * 二分法
+     * 从矩阵 matrix 左下角元素（索引设为 (i, j) ）开始遍历，并与目标值对比：
+     * 当 matrix[i][j] > target 时，执行 i-- ，即消去第 i 行元素；
+     * 当 matrix[i][j] < target 时，执行 j++ ，即消去第 j 列元素；
+     * 当 matrix[i][j] = target 时，返回 true ，代表找到目标值。
+     * 若行索引或列索引越界，则代表矩阵中无目标值，返回 false 。
+     * <p>
+     * 作者：jyd
+     * 链接：https://leetcode-cn.com/problems/er-wei-shu-zu-zhong-de-cha-zhao-lcof/solution/mian-shi-ti-04-er-wei-shu-zu-zhong-de-cha-zhao-zuo/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        int i = matrix.length - 1, j = 0;// （i,j）左下角坐标
+        while (i > 0 && j <= matrix[0].length) {
+            if (matrix[i][j] > target) i--;
+            else if (matrix[i][j] < target) j++;
+            return true;
+        }
+        return false;
+    }
+}
