0bd28719e6f8643705b9ed166e63b6981f4aed0b
nateshao <shaotongjie@gmail.com>
2022-03-14 23:30:36 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/zijie_code_66/Day04_twoSum.java
+++ algo-notes/src/main/java/com/nateshao/zijie_code_66/Day04_twoSum.java
@@ -1,5 +1,8 @@
 package com.nateshao.zijie_code_66;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @date Created by 邵桐杰 on 2022/3/14 23:19
  * @微信公众号 千羽的编程时光
@@ -51,5 +54,35 @@ public class Day04_twoSum {
             }
         }
         return new int[0];
+    }
+
+    /**
+     * 思路
+     * 标签：哈希映射
+     * 这道题本身如果通过暴力遍历的话也是很容易解决的，时间复杂度在 O(n2)O(n2)
+     * 由于哈希查找的时间复杂度为 O(1)，所以可以利用哈希容器 map 降低时间复杂度
+     * 遍历数组 nums，i 为当前下标，每个值都判断map中是否存在 target-nums[i] 的 key 值
+     * 如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
+     * 如果最终都没有结果则抛出异常
+     * 时间复杂度：$$
+     *
+     * 作者：guanpengchn
+     * 链接：https://leetcode-cn.com/problems/two-sum/solution/jie-suan-fa-1-liang-shu-zhi-he-by-guanpengchn/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i< nums.length; i++) {
+            if(map.containsKey(target - nums[i])) {
+                return new int[] {map.get(target-nums[i]),i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+
     }
 }
