7431b062b4fd70c365900a8a71ab7f49bfca2a51
nateshao <shaotongjie@gmail.com>
2022-03-15 23:03:22 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/zijie_code_66/Day04_trap.java
@@ -0,0 +1,58 @@
+package com.nateshao.zijie_code_66;
+
+/**
+ * @date Created by 邵桐杰 on 2022/3/15 22:43
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode-cn.com/problems/trapping-rain-water/
+ * 42. 接雨水
+ * 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ * 解释：上面是由数组 [0,1,0,2,1,0,1,3,2,1,2,1] 表示的高度图，在这种情况下，可以接 6 个单位的雨水（蓝色部分表示雨水）。
+ * 示例 2：
+ * <p>
+ * 输入：height = [4,2,0,3,2,5]
+ * 输出：9
+ */
+public class Day04_trap {
+
+    public static void main(String[] args) {
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2};
+        int trap = trap(height);
+        System.out.println("trap = " + trap);
+    }
+    public static int trap(int[] height) {
+        int sum = 0;
+        int max_left = 0;
+        int max_right = 0;
+        int left = 1;
+        int right = height.length - 2; // 加右指针进去
+        for (int i = 1; i < height.length - 1; i++) {
+            //从左到右更
+            if (height[left - 1] < height[right + 1]) {
+                max_left = Math.max(max_left, height[left - 1]);
+                int min = max_left;
+                if (min > height[left]) {
+                    sum = sum + (min - height[left]);
+                }
+                left++;
+                //从右到左更
+            } else {
+                max_right = Math.max(max_right, height[right + 1]);
+                int min = max_right;
+                if (min > height[right]) {
+                    sum = sum + (min - height[right]);
+                }
+                right--;
+            }
+        }
+        return sum;
+    }
+}
