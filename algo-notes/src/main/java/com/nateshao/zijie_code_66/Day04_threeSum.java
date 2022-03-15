af428e8f9d1c4611413edc904cbc81c176f573d8
nateshao <shaotongjie@gmail.com>
2022-03-15 17:08:55 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/zijie_code_66/Day04_threeSum.java
@@ -0,0 +1,63 @@
+package com.nateshao.zijie_code_66;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @date Created by 邵桐杰 on 2022/3/15 10:53
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 15. 三数之和
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * 示例 2：
+ * <p>
+ * 输入：nums = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：nums = [0]
+ * 输出：[]
+ */
+public class Day04_threeSum {
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> lists = threeSum(nums);
+        lists.stream().forEach(list-> System.out.println("list = " + list));
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);// 排序，nums变成递增数组
+        List<List<Integer>> res = new ArrayList<>();
+        for (int k = 0; k < nums.length - 2; k++) {//nums.length - 2是为了保证后面还可以存两个数字
+            if (nums[k] > 0) break;// 若nums[k]大于0，则后面的数字也是大于零（排序后是递增的）
+            if (k > 0 && nums[k] == nums[k - 1]) continue;// nums[k]值重复了，去重
+            // 定义左右指针
+            int left = k + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[k] + nums[left] + nums[right];
+                if (sum < 0) {
+                    while (left < right && nums[left] == nums[++left]) ;//左指针前进并且去重
+                } else if (sum > 0) {
+                    while (left < right && nums[right] == nums[--right]) ;// 右指针前进并且去重
+                } else {
+                    res.add(new ArrayList<Integer>(Arrays.asList(nums[k], nums[left], nums[right])));
+                    while (left < right && nums[left] == nums[++left]) ;
+                    while (left < right && nums[right] == nums[--right]) ;
+                }
+            }
+        }
+        return res;
+    }
+
+}
--- algo-notes/src/main/java/com/nateshao/zijie_code_66/Day04_twoSum.java
+++ algo-notes/src/main/java/com/nateshao/zijie_code_66/Day04_twoSum.java
@@ -1,5 +1,6 @@
 package com.nateshao.zijie_code_66;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -40,11 +41,13 @@ public class Day04_twoSum {
 
     /**
      * 两数之和 ：暴力破解
+     *
      * @param nums
      * @param target
      * @return
      */
     public static int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[0];
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -65,26 +68,63 @@ public class Day04_twoSum {
      * 如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
      * 如果最终都没有结果则抛出异常
      * 时间复杂度：$$
-     *
+     * <p>
      * 作者：guanpengchn
      * 链接：https://leetcode-cn.com/problems/two-sum/solution/jie-suan-fa-1-liang-shu-zhi-he-by-guanpengchn/
      * 来源：力扣（LeetCode）
      * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
      * @param nums
      * @param target
      * @return
      */
     public int[] twoSum2(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[0];
         Map<Integer, Integer> map = new HashMap<>();
         // 遍历数组 nums，i 为当前下标，每个值都判断map中是否存在 target-nums[i] 的 key 值
-        for(int i = 0; i< nums.length; i++) {
-            if(map.containsKey(target - nums[i])) {
-                return new int[] {map.get(target-nums[i]),i};
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
             }
             //如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
             map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No two sum solution");
+        return new int[]{};
+    }
 
+    /**
+     * 双指针
+     *https://leetcode-cn.com/problems/two-sum/solution/liang-shu-zhi-he-by-wangdong-a/
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum3(int[] nums, int target) {
+        int[] numsbak = new int[nums.length];
+        System.arraycopy(nums, 0, numsbak, 0, nums.length);
+        Arrays.sort(nums);
+        int a = -1, b = -1;
+        for (int i = 0, j = nums.length - 1; i < nums.length - 1 && j > i; ) {
+            if (nums[i] + nums[j] < target) {
+                i++;
+            } else if (nums[i] + nums[j] > target) {
+                j--;
+            } else {
+                a = nums[i];
+                b = nums[j];
+                break;
+            }
+        }
+
+        int x = -1, y = -1;
+        for (int i = 0; i < numsbak.length; i++) {
+            if (numsbak[i] == a && x == -1) {
+                x = i;
+                continue;
+            }
+            if (numsbak[i] == b && y == -1) y = i;
+            if (x > -1 && y > -1) break;
+        }
+        return new int[]{x, y};
     }
 }
