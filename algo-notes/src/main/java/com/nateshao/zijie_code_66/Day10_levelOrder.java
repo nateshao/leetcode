1273affc5a4e97a4361fa4c04fac592f11d6c9ad
nateshao <shaotongjie@gmail.com>
2022-03-17 12:57:30 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/zijie_code_66/Day10_levelOrder.java
+++ algo-notes/src/main/java/com/nateshao/zijie_code_66/Day10_levelOrder.java
@@ -1,7 +1,10 @@
 package com.nateshao.zijie_code_66;
 
+
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * @date Created by 邵桐杰 on 2022/3/17 9:47
@@ -29,8 +32,24 @@ import java.util.List;
  */
 public class Day10_levelOrder {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> res = new LinkedList<>();
         if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        // while 循环控制从上向下一层层遍历
+        while (!q.isEmpty()) {
+            int sz = q.size();
+            // 记录这一层的节点值
+            List<Integer> level = new LinkedList<>();
+            // for 循环控制每一层从左向右遍历
+            for (int i = 0; i < sz; i++) {
+                TreeNode cur = q.poll();
+                level.add(cur.val);
+                if (cur.left != null) q.offer(cur.left);
+                if (cur.right != null) q.offer(cur.right);
+            }
+            res.add(level);
+        }
         return res;
     }
 
