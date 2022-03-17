f0f29d3229de4bb9f60379fb74da44c936450a08
nateshao <shaotongjie@gmail.com>
2022-03-17 20:12:30 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/one_question_per_day/code_2022/march/Solution_2022_03_17_longestWord.java
@@ -0,0 +1,35 @@
+package com.nateshao.one_question_per_day.code_2022.march;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @date Created by 邵桐杰 on 2022/3/17 20:08
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Solution_2022_03_17_longestWord {
+    public static void main(String[] args) {
+        String[] words = {"w", "wo", "wor", "worl", "world"};
+        String s = longestWord(words);
+        System.out.println("s = " + s);
+    }
+
+    public static String longestWord(String[] words) {
+        Arrays.sort(words);
+        Set<String> set = new HashSet<>();
+        String res = "";
+        for (String s : words) {
+            if (s.length() == 1 || set.contains(s.substring(0, s.length() - 1))) {
+                res = s.length() > res.length() ? s : res;
+                set.add(res);
+            }
+        }
+        return res;
+    }
+}
