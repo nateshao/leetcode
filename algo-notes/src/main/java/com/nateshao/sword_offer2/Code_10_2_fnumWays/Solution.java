61cbfa8bd138be301a8aed0a74d12a36a7ae410e
nateshao <shaotongjie@gmail.com>
2022-03-30 18:46:34 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer2/Code_10_2_fnumWays/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_10_2_fnumWays/Solution.java
@@ -39,4 +39,23 @@ public class Solution {
         }
         return a;
     }
+
+    /**
+     * 动态规划
+     * @param n
+     * @return
+     */
+    public int numWays2(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 1000_000_007;
+        }
+        return dp[n];
+    }
+
 }
