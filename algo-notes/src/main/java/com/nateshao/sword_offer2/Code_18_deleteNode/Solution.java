763fbd1130c1dc1e99e7593149dce11c5f88135d
nateshao <shaotongjie@gmail.com>
2022-04-06 09:56:17 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_18_deleteNode/Solution.java
@@ -0,0 +1,63 @@
+package com.nateshao.sword_offer2.Code_18_deleteNode;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/6 9:38
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Solution {
+    /**
+     * 解法一：递归
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode deleteNode(ListNode head, int val) {
+        if (head == null) return null;
+        if (head.val == val) return head.next;
+        head.next = deleteNode(head.next, val);
+        return head;
+    }
+
+    /**
+     * 解法二，双指针
+     *
+     * 特例处理： 当应删除头节点 head 时，直接返回 head.next 即可。
+     * 初始化： pre = head , cur = head.next 。
+     * 定位节点： 当 cur 为空 或 cur 节点值等于 val 时跳出。
+     * 保存当前节点索引，即 pre = cur 。
+     * 遍历下一节点，即 cur = cur.next 。
+     * 删除节点： 若 cur 指向某节点，则执行 pre.next = cur.next ；若 cur 指向 null ，代表链表中不包含值为 val 的节点。
+     * 返回值： 返回链表头部节点 head 即可。
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode deleteNode2(ListNode head, int val) {
+        if (head.val == val) return head.next;
+        ListNode pre = head, cur = head.next;
+        while (cur != null && cur.val != val) {
+            pre = cur;
+            cur = cur.next;
+        }
+        if (cur != null) pre.next = cur.next;
+        return head;
+
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_19_isMatch/Solution.java
@@ -0,0 +1,27 @@
+package com.nateshao.sword_offer2.Code_19_isMatch;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/6 9:52
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 19. 正则表达式匹配
+ * 请实现一个函数用来匹配包含'. '和'*'的正则表达式。模式中的字符'.'表示任意一个字符，
+ * 而'*'表示它前面的字符可以出现任意次（含0次）。在本题中，匹配是指字符串的所有字符匹配整个模式。
+ * 例如，字符串"aaa"与模式"a.a"和"ab*ac*a"匹配，但与"aa.a"和"ab*a"均不匹配。
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        String s = "aa";
+        String p = "a";
+        System.out.println("isMatch(s,p) = " + isMatch(s, p));
+    }
+
+    public static boolean isMatch(String s, String p) {
+        return s.matches(p);
+    }
+}
