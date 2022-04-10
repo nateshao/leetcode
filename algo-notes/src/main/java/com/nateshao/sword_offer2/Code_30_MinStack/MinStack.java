cd4909bf20e1feedd08a08bbb73422f68f478ccd
nateshao <shaotongjie@gmail.com>
2022-04-10 09:26:48 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/sword_offer2/Code_30_MinStack/MinStack.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_30_MinStack/MinStack.java
@@ -15,6 +15,10 @@ import java.util.Stack;
  * 定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，
  * 调用 min、push 及 pop 的时间复杂度都是 O(1)。
  *
+ * 思路：本题难点： 将 min() 函数复杂度降为 O(1) ，可通过建立辅助栈实现；
+ * 1. 数据栈 A ： 栈 A 用于存储所有元素，保证入栈 push() 函数、出栈 pop() 函数、获取栈顶 top() 函数的正常逻辑。
+ * 2. 辅助栈 B ： 栈 B 中存储栈 A 中所有 非严格降序 的元素，则栈 A 中的最小元素始终对应栈 B 的栈顶元素，即 min() 函数只需返回栈 B 的栈顶元素即可。
+ * 因此，只需设法维护好 栈 B 的元素，使其保持非严格降序，即可实现 min() 函数的 O(1)O(1) 复杂度。
  *
  */
 public class MinStack {
@@ -31,18 +35,20 @@ public class MinStack {
         // 数据栈，进栈
         stack1.push(x);
         // 如果记录当前数据栈中最小值的辅助栈为空，或者最小值小于 x，则将 x 设置为最小值，即进辅助栈
-        if (stack2.isEmpty() || stack2.peek() >= x) stack2.push(x);
+        // 辅助栈stack2为空，且栈顶元素大于进栈元素x，将x压栈
+        if (stack2.isEmpty() || stack2.peek() >= x) stack2.push(x);// stack2存最小值
     }
 
-    public void pop() {
+    public void pop() { // 出栈
+        // 如果数据栈出栈元素==辅助栈的栈顶元素，数据栈stack2出栈
         if (stack1.pop().equals(stack2.peek())) stack2.pop();
     }
 
     public int top() {
-        return stack1.peek();
+        return stack1.peek();// 返回栈顶的元素但不移除它。Stack的pop方法是会移除的。
     }
 
     public int min() {
-        return stack2.peek();
+        return stack2.peek();// 返回
     }
 }
