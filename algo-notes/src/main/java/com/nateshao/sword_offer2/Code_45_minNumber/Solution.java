f1cc776c3e5e0a0f71a1504fa30a664822666a68
nateshao <shaotongjie@gmail.com>
2022-04-18 16:33:35 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_45_minNumber/Solution.java
@@ -0,0 +1,68 @@
+package com.nateshao.sword_offer2.Code_45_minNumber;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/17 20:07
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 45. 把数组排成最小的数
+ * 输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
+ *
+ * 示例 1:
+ *
+ * 输入: [10,2]
+ * 输出: "102"
+ * 示例 2:
+ *
+ * 输入: [3,30,34,5,9]
+ * 输出: "3033459"
+ */
+public class Solution {
+
+
+    public  String minNumber(int[] nums) {
+        if (nums == null || nums.length == 0) return "";
+        int len = nums.length;
+        String[] str = new String[len];
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; i++) {
+            str[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(str, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                String c1 = s1 + s2;
+                String c2 = s2 + s1;
+                return c1.compareTo(c2);
+            }
+        });
+        for (int i = 0; i < len; i++) {
+            sb.append(str[i]);
+        }
+        return sb.toString();
+    }
+
+    /**
+     * 本质上是排序
+     * @param nums
+     * @return
+     */
+    public String minNumber2(int[] nums) {
+        String[] strs = new String[nums.length];
+        for(int i = 0; i < nums.length; i++)
+            strs[i] = String.valueOf(nums[i]);// 转换string类型
+        Arrays.sort(strs, (x, y) -> (x + y).compareTo(y + x));//排序
+        StringBuilder res = new StringBuilder();
+        for(String s : strs)
+            res.append(s);
+        return res.toString();
+    }
+
+
+}
