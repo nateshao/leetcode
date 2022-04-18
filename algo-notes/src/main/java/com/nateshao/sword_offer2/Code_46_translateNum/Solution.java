5eae1d4d0dbbbca254c252d69dbeef8f458c7277
nateshao <shaotongjie@gmail.com>
2022-04-18 18:47:02 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_46_translateNum/Solution.java
@@ -0,0 +1,28 @@
+package com.nateshao.sword_offer2.Code_46_translateNum;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/18 17:20
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Solution {
+    public int translateNum(int num) {
+        if (num <= 9) {
+            return 1;
+        }
+        //获取输入数字的余数，然后递归的计算翻译方法
+        int ba = num % 100;
+        //如果小于等于9或者大于等于26的时候，余数不能按照2位数字组合，比如56，只能拆分为5和6；反例25，可以拆分为2和5，也可以作为25一个整体进行翻译。
+        if (ba <= 9 || ba >= 26) {
+            return translateNum(num / 10);
+        }
+        // ba=[10, 25]时，既可以当做一个字母，也可以当做两个字母
+        else {
+            return translateNum(num / 10) + translateNum(num / 100);
+        }
+    }
+}
---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_47_maxValue/Solution.java
@@ -0,0 +1,38 @@
+package com.nateshao.sword_offer2.Code_47_maxValue;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/18 18:46
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 47. 礼物的最大价值
+ * 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * [
+ *   [1,3,1],
+ *   [1,5,1],
+ *   [4,2,1]
+ * ]
+ * 输出: 12
+ * 解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+ *
+ */
+public class Solution {
+    public int maxValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[j] = Math.max(dp[j], dp[j - 1]) + grid[i - 1][j - 1];
+            }
+        }
+
+        return dp[n];
+    }
+}
