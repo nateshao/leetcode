4fc06eedff91eb2281556c7ec7188bb4b0db13c6
nateshao <shaotongjie@gmail.com>
2022-04-19 20:36:35 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/Code_48_lengthOfLongestSubstring/Solution.java
@@ -0,0 +1,122 @@
+package com.nateshao.sword_offer2.Code_48_lengthOfLongestSubstring;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/19 19:38
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode-cn.com/problems/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof/
+ * 剑指 Offer 48. 最长不含重复字符的子字符串
+ * 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ * <p>
+ * 输入: "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ * 输入: "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+public class Solution {
+
+    /**
+     * 滑动窗口
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        int res = 0;
+        Set<Character> set = new HashSet<>();
+        for(int l = 0, r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            while(set.contains(c)) {
+                set.remove(s.charAt(l++));
+            }
+            set.add(c);
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
