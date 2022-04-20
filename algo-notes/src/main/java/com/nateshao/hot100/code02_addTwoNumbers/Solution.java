09fb7fcfd9fffac33d5f022859fd9fea875d9ea4
nateshao <shaotongjie@gmail.com>
2022-04-20 13:01:08 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/hot100/code02_addTwoNumbers/Solution.java
+++ algo-notes/src/main/java/com/nateshao/hot100/code02_addTwoNumbers/Solution.java
@@ -1,5 +1,7 @@
 package com.nateshao.hot100.code02_addTwoNumbers;
 
+import com.nateshao.hot100.include.ListNode;
+
 /**
  * @date Created by 邵桐杰 on 2022/4/20 11:20
  * @微信公众号 千羽的编程时光
@@ -10,4 +12,33 @@ package com.nateshao.hot100.code02_addTwoNumbers;
  * Description:
  */
 public class Solution {
+    /**
+     * 1，按位求和
+     * 2，进位
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+    }
+
+
+//    public class ListNode {
+//        int val;
+//        ListNode next;
+//
+//        ListNode() {
+//        }
+//
+//        ListNode(int val) {
+//            this.val = val;
+//        }
+//
+//        ListNode(int val, ListNode next) {
+//            this.val = val;
+//            this.next = next;
+//        }
+//    }
 }
+
