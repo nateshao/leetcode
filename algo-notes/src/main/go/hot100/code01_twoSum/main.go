b08742ceb5cc5e2b9a20f366781c86865a3d1dd4
nateshao <shaotongjie@gmail.com>
2022-04-20 11:15:55 +0800

    死磕算法系列

---
+++ algo-notes/src/main/go/hot100/code01_twoSum/main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+
+}
+
+// 方法一：暴力枚举
+func twoSum(nums []int, target int) []int {
+	for i, x := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if x+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
+
+// 方法二：哈希表
+func twoSum2(nums []int, target int) []int {
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if p, ok := hashTable[target-x]; ok {
+			return []int{p, i}
+		}
+		hashTable[x] = i
+	}
+	return nil
+}
