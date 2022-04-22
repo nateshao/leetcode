6cde253b91e3d811907b82cbb06af582e3acab0f
nateshao <shaotongjie@gmail.com>
2022-04-22 22:54:54 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/hot100/code94_inorderTraversal/Solution.java
@@ -0,0 +1,42 @@
+package com.nateshao.hot100.code94_inorderTraversal;
+
+import com.nateshao.hot100.include.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/22 22:48
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitee.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ * 94. 二叉树的中序遍历
+ * 给定一个二叉树的根节点 root ，返回 它的 中序 遍历 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 示例 2：
+ * <p>
+ * 输入：root = []
+ * 输出：[]
+ * 示例 3：
+ * <p>
+ * 输入：root = [1]
+ * 输出：[1]
+ */
+public class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        inorderTraversal(root.left);
+        res.addAll(inorderTraversal(root.left));
+        res.add(root.val);
+        res.addAll(inorderTraversal(root.right));
+        return res;
+    }
+}
