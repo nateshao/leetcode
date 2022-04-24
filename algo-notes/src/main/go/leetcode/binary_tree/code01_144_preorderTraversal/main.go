3b5e7ae0ad682a67844b76cc2b08af6681e8a8ef
nateshao <shaotongjie@gmail.com>
2022-04-24 08:52:31 +0800

    死磕算法系列

---
+++ algo-notes/src/main/go/leetcode/binary_tree/code01_144_preorderTraversal/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+func preorderTraversal(root *TreeNode) (vals []int) {
+	var preorder func(*TreeNode)
+	preorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		vals = append(vals, node.Val)
+		preorder(node.Left)
+		preorder(node.Right)
+	}
+	preorder(root)
+	return
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
