60725a3924fe9fa2cfd392c9921f4cb37aac89ec
nateshao <shaotongjie@gmail.com>
2022-04-24 17:59:51 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/leetcode/binary_tree/code06_102_levelOrder/Solution.java
@@ -0,0 +1,82 @@
+package com.nateshao.leetcode.binary_tree.code06_102_levelOrder;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @date Created by 邵桐杰 on 2022/4/24 17:07
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode-cn.com/problems/binary-tree-level-order-traversal/
+ * 102. 二叉树的层序遍历
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ * 示例 2：
+ * <p>
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * 示例 3：
+ * <p>
+ * 输入：root = []
+ * 输出：[]
+ */
+public class Solution {
+
+    /**
+     * 输入一棵二叉树的根节点，层序遍历这棵二叉树
+     * 深度优先，队列
+     *
+     * @param root
+     * @return
+     */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            // 从上到下遍历二叉树的每一层
+            int size = queue.size();
+            // 记录这一层的节点值
+            List<Integer> level = new LinkedList<>();
+            // 从左到右遍历每一层的每个节点
+            for (int i = 0; i < size; i++) {
+                TreeNode cur = queue.poll();
+                level.add(cur.val);
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
