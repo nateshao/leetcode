b7d116ba535ad39f6e1a17fb9711d520c5993fae
nateshao <shaotongjie@gmail.com>
2022-04-28 23:15:51 +0800

    死磕算法系列

--- algo-notes/src/main/java/com/nateshao/albaba_03_14/Leetcode905.java
+++ algo-notes/src/main/java/com/nateshao/albaba_03_14/Leetcode905.java
@@ -23,23 +23,15 @@ public class Leetcode905 {
      * @return
      */
     public int[] sortArrayByParity(int[] nums) {
-        int left = -1, right = nums.length - 1;
+        int left = 0;
+        int right = nums.length - 1;
         while (left < right) {
-            while (left < right && nums[left] % 2 == 0) {
-                left++;
-            }
-            while (left < right && nums[right] % 2 == 1) {
-                right--;
-            }
-            if (left < right) {
-                int temp = nums[left];
-                nums[left] = nums[right];
-                nums[right] = temp;
-                left++;
-                right--;
-            }
+            while (left < right && nums[left] % 2 != 0) left++;
+            while (left < right && nums[right] % 2 == 0) right--;
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
         }
         return nums;
-
     }
 }
