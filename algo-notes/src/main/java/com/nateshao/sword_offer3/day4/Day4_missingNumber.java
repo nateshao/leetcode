9783822669d31846a60bf54d8923f50e6d6606aa
nateshao <shaotongjie@gmail.com>
2022-05-29 23:05:30 +0800

    死磕算法系列

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day4/Day4_missingNumber.java
@@ -0,0 +1,53 @@
+package com.nateshao.sword_offer3.day4;
+
+import java.util.Arrays;
+
+/**
+ * @date Created by 邵桐杰 on 2022/5/29 22:56
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Day4_missingNumber {
+    public int missingNumber(int[] nums) {
+        // 理论的和减去实际的和即为缺少的数字
+        //计算出0-n的和  n*(n+1)/2
+        int sum = nums.length * (nums.length + 1) / 2;
+        return sum - Arrays.stream(nums).sum();
+
+    }
+
+
+    /**
+     * 虽然是O(N)但我感觉这个看着好舒服啊
+     *
+     * @param nums
+     * @return
+     */
+    public int missingNumber2(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums[i]) return i;
+        }
+        return nums.length;
+    }
+
+    /**
+     * 二分
+     *
+     * @param nums
+     * @return
+     */
+    public int missingNumber3(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == mid) left = mid + 1;
+            else right = mid - 1;
+        }
+        return left;
+    }
+
+}
---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day4/Day4_search.java
@@ -0,0 +1,85 @@
+package com.nateshao.sword_offer3.day4;
+
+import java.util.ArrayList;
+
+/**
+ * @date Created by 邵桐杰 on 2022/5/29 20:55
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 53 - I. 在排序数组中查找数字 I
+ * 统计一个数字在排序数组中出现的次数。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: nums = [5,7,7,8,8,10], target = 8
+ * 输出: 2
+ * <p>
+ * 示例 2:
+ * 输入: nums = [5,7,7,8,8,10], target = 6
+ * 输出: 0
+ */
+public class Day4_search {
+    /**
+     * 自己想的：
+     * 遍历一遍数组，如果nums[i] == target,res++
+     * 返回res
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 二分查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search2(int[] nums, int target) {
+        int low = 0;
+        int hight = nums.length - 1;
+        while (low <= hight) {
+            int mid = low + (hight - low) / 2;
+            if (nums[mid] < target) {
+                low=mid+1;
+            }else if (nums[mid]>target){
+                hight=mid-1;
+            }else {
+                if (nums[low]==nums[hight]){
+                    return hight-low+1;
+                }else if (nums[low]<target){
+                    low++;
+                }else {
+                    hight--;
+                }
+            }
+        }
+        return 0;
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
