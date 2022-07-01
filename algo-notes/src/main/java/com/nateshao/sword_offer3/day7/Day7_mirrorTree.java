f71a9b7f80c12be75fb4f3a6ebe0da1ddf53f343
nateshao <shaotongjie@gmail.com>
2022-07-01 23:55:46 +0800

    算法，我待你如初恋

--- algo-notes/src/main/java/com/nateshao/sword_offer3/day7/Day7_mirrorTree.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day7/Day7_mirrorTree.java
@@ -10,23 +10,23 @@ package com.nateshao.sword_offer3.day7;
  * Description:
  * 剑指 Offer 27. 二叉树的镜像
  * 请完成一个函数，输入一个二叉树，该函数输出它的镜像。
- *
+ * <p>
  * 例如输入：
- *
- *      4
- *    /   \
- *   2     7
- *  / \   / \
+ * <p>
+ * 4
+ * /   \
+ * 2     7
+ * / \   / \
  * 1   3 6   9
  * 镜像输出：
- *
- *      4
- *    /   \
- *   7     2
- *  / \   / \
+ * <p>
+ * 4
+ * /   \
+ * 7     2
+ * / \   / \
  * 9   6 3   1
  * 示例 1：
- *
+ * <p>
  * 输入：root = [4,2,7,1,3,6,9]
  * 输出：[4,7,2,9,6,3,1]
  */
@@ -38,6 +38,30 @@ public class Day7_mirrorTree {
         root.left = mirrorTree(root.right);      // left = right
         root.right = mirrorTree(node);           // right = temp
         return root;                             // return root
+    }
+
+    /**
+     * 226. 翻转二叉树
+     * 给你一棵二叉树的根节点 root ，翻转这棵二叉树，并返回其根节点。
+     * 示例 1：
+     * 输入：root = [4,2,7,1,3,6,9]
+     * 输出：[4,7,2,9,6,3,1]
+     * 示例 2：
+     * 输入：root = [2,1,3]
+     * 输出：[2,3,1]
+     * 示例 3：
+     * 输入：root = []
+     * 输出：[]
+     *
+     * @param root
+     * @return
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode node = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(node);
+        return root;
     }
 
     public class TreeNode {
