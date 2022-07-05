ffd7b6ac88dfb70dc07c69893c53b72efefab8ce
nateshao <shaotongjie@gmail.com>
2022-07-05 21:51:07 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day8/Day8_numWays.java
@@ -0,0 +1,43 @@
+package com.nateshao.sword_offer3.day8;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/5 21:42
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 10- II. 青蛙跳台阶问题
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n 级的台阶总共有多少种跳法。
+ *
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ *
+ * 示例 1：
+ *
+ * 输入：n = 2
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：n = 7
+ * 输出：21
+ * 示例 3：
+ *
+ * 输入：n = 0
+ * 输出：1
+ */
+public class Day8_numWays {
+    public int numWays(int n) {
+        if (n == 0 || n == 1) return 1;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] %= 1000_000_007;
+        }
+        return dp[n];
+
+    }
+
+}
