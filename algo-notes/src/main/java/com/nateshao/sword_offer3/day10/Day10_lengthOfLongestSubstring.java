ba533f6f42ae38924bac3afe1772f662462f0061
nateshao <shaotongjie@gmail.com>
2022-07-11 22:27:10 +0800

    算法，我待你如初恋

--- algo-notes/src/main/java/com/nateshao/sword_offer3/day10/Day10_lengthOfLongestSubstring.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day10/Day10_lengthOfLongestSubstring.java
@@ -1,5 +1,7 @@
 package com.nateshao.sword_offer3.day10;
 
+import java.util.HashSet;
+
 /**
  * @date Created by 邵桐杰 on 2022/7/10 21:38
  * @微信公众号 千羽的编程时光
@@ -14,25 +16,36 @@ package com.nateshao.sword_offer3.day10;
  * 输入: "abcabcbb"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
- *
+ * <p>
  * 示例 2:
  * 输入: "bbbbb"
  * 输出: 1
  * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
- *
+ * <p>
  * 示例 3:
  * 输入: "pwwkew"
  * 输出: 3
  * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
- *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
  */
 public class Day10_lengthOfLongestSubstring {
     /**
+     * 滑动窗口
      *
      * @param s
      * @return
      */
     public int lengthOfLongestSubstring(String s) {
-        return 0;
+        int l = 0, r = 0, res = 0;// 存放结果
+        HashSet<Object> set = new HashSet<>();
+        while (r < s.length()) {
+            char c = s.charAt(r++);
+            while (set.contains(c)) {
+                set.remove(s.charAt(l++));
+            }
+            set.add(c);
+            res = Math.max(res, set.size());
+        }
+        return res;
     }
 }
