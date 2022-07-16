2d269f0d142d0831e14c5334168da182c000eac8
nateshao <shaotongjie@gmail.com>
2022-07-16 22:35:48 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day13/Day13_reverseWords.java
@@ -0,0 +1,59 @@
+package com.nateshao.sword_offer3.day13;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/13 14:20
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 58 - I. 翻转单词顺序
+ * 输入一个英文句子，翻转句子中单词的顺序，但单词内字符的顺序不变。
+ * 为简单起见，标点符号和普通字母一样处理。例如输入字符串"I am a student. "，则输出"student. a am I"。
+ * 示例 1：
+ *
+ * 输入: "the sky is blue"
+ * 输出: "blue is sky the"
+ * 示例 2：
+ *
+ * 输入: "  hello world!  "
+ * 输出: "world! hello"
+ * 解释: 输入字符串可以在前面或者后面包含多余的空格，但是反转后的字符不能包括。
+ * 示例 3：
+ *
+ * 输入: "a good   example"
+ * 输出: "example good a"
+ * 解释: 如果两个单词间有多余的空格，将反转后单词间的空格减少到只含一个。
+ */
+public class Day13_reverseWords {
+    /**
+     * k神
+     * 删除尾巴空格
+     * 倒序遍历
+     * @param s
+     * @return
+     */
+    public String reverseWords(String s) {
+        String[] strs = s.trim().split(" "); // 删除首尾空格，分割字符串
+        StringBuilder res = new StringBuilder();
+        for(int i = strs.length - 1; i >= 0; i--) { // 倒序遍历单词列表
+            if(strs[i].equals("")) continue; // 遇到空单词则跳过
+            res.append(strs[i] + " "); // 将单词拼接至 StringBuilder
+        }
+        return res.toString().trim(); // 转化为字符串，删除尾部空格，并返回
+    }
+
+    public String reverseWords2(String s) {
+        String[] str = s.trim().split(" ");
+        StringBuffer stringBuffer = new StringBuffer();
+        // 从尾巴开始遍历
+        for (int i = str.length - 1;i>=0; i--) {
+            if (str[i].equals(""))continue;
+            if (i==0)stringBuffer.append(str[i].trim());
+            else stringBuffer.append(str[i].trim()).append(" ");
+
+        }
+        return stringBuffer.toString();
+    }
+}
