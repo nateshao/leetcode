a0739397e1f05ef0395acd05e535b801d4fa1fa5
nateshao <shaotongjie@gmail.com>
2022-07-20 10:25:43 +0800

    算法，我待你如初恋

--- algo-notes/src/main/java/com/nateshao/sword_offer3/day18/Day18_isBalanced.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day18/Day18_isBalanced.java
@@ -10,11 +10,32 @@ package com.nateshao.sword_offer3.day18;
  * Description:
  */
 public class Day18_isBalanced {
-
+    /**
+     * 是否平衡，
+     * 1，获取二叉树的深度（深度优先遍历-递归）
+     * 2，如果左右最大深度大于 1，就不是平衡二叉树
+     *
+     * @param root
+     * @return
+     */
     public boolean isBalanced(TreeNode root) {
-        if (root == null) return true;
-
+        maxDepth(root);
+        return isBalanced;
     }
+
+    boolean isBalanced = true;
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        // 递归遍历左右子树
+        int leftMaxDepth = maxDepth(root.left);
+        int rightMaxDepth = maxDepth(root.right);
+        if (Math.abs((leftMaxDepth - rightMaxDepth)) > 1) {
+            isBalanced = false;
+        }
+        return Math.max(leftMaxDepth, rightMaxDepth) + 1;
+    }
+
+
 
     public class TreeNode {
         int val;
