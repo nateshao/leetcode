03d149a8aeb615330199cdc5f8155121044ee407
nateshao <shaotongjie@gmail.com>
2022-07-20 10:08:57 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day18/Day18_maxDepth.java
@@ -0,0 +1,75 @@
+package com.nateshao.sword_offer3.day18;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/20 9:48
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 55 - I. 二叉树的深度
+ * 输入一棵二叉树的根节点，求该树的深度。从根节点到叶节点依次经过的节点（含根、叶节点）形成树的一条路径，
+ * 最长路径的长度为树的深度。
+ * <p>
+ * 例如：
+ * <p>
+ * 给定二叉树 [3,9,20,null,null,15,7]，
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回它的最大深度 3 。
+ */
+public class Day18_maxDepth {
+    /**
+     * 深度优先遍历
+     * 递归
+     * @param root
+     * @return
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    /**
+     * 队列
+     *
+     * @param root
+     * @return
+     */
+    public int maxDepth2(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int res = 0;
+        while (!queue.isEmpty()) {
+            res++;
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) queue.add(node.left);
+                if (node.right != null) queue.add(node.right);
+            }
+        }
+        return res;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
