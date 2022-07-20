f0c1ccb73beebbae463a6746a9b9d5772116cae2
nateshao <shaotongjie@gmail.com>
2022-07-20 11:16:20 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day19/Day19_sumNums.java
@@ -0,0 +1,26 @@
+package com.nateshao.sword_offer3.day19;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/20 10:27
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Day19_sumNums {
+    public static void main(String[] args) {
+        System.out.println("sumNums(10) = " + sumNums(10));
+    }
+    /**
+     * 递归
+     * n > 1 && sumNums(n - 1) // 当 n = 1 时 n > 1 不成立 ，此时 “短路” ，终止后续递归
+     * @param n
+     * @return
+     */
+    public static int sumNums(int n) {
+        boolean x = n > 1 && (n += sumNums(n - 1)) > 0;
+        return n;
+    }
+}
