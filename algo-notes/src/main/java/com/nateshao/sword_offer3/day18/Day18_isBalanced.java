0954586faabb00e6db160754c523b5d10a522e82
nateshao <shaotongjie@gmail.com>
2022-07-20 10:11:58 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day18/Day18_isBalanced.java
@@ -0,0 +1,28 @@
+package com.nateshao.sword_offer3.day18;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/20 10:11
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Day18_isBalanced {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
