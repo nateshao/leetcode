3e7e83e2c771984736370d60d6ebc9b3f4fddc4d
nateshao <shaotongjie@gmail.com>
2022-07-22 23:56:47 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day21/Day21_hammingWeight.java
@@ -0,0 +1,21 @@
+package com.nateshao.sword_offer3.day21;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/22 23:47
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Day21_hammingWeight {
+    public int hammingWeight(int n) {
+        int res = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            res++;
+        }
+        return res;
+    }
+}
