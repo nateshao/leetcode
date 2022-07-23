e316dd7cfda291393bf5748bb70f3e419c888eed
nateshao <shaotongjie@gmail.com>
2022-07-23 10:57:57 +0800

    算法，我待你如初恋

--- algo-notes/src/main/java/com/nateshao/sword_offer3/day21/Day21_add.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day21/Day21_add.java
@@ -8,6 +8,12 @@ package com.nateshao.sword_offer3.day21;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  * Description:
+ * 剑指 Offer 65. 不用加减乘除做加法
+ * 写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+ * 示例:
+ *
+ * 输入: a = 1, b = 1
+ * 输出: 2
  */
 public class Day21_add {
 
@@ -25,7 +31,7 @@ public class Day21_add {
 
     public int add2(int a, int b) {
         if (a == 0 || b == 0) return a == 0 ? a : b;
-        // 假设a = 1001
+        // 假设 a = 1001
         // 假设 b = 0101
         // 求和 1100
         int sum = a ^ b;
---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day22/Day22_singleNumbers.java
@@ -0,0 +1,46 @@
+package com.nateshao.sword_offer3.day22;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/23 9:47
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 剑指 Offer 56 - I. 数组中数字出现的次数
+ * 一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ * 要求时间复杂度是O(n)，空间复杂度是O(1)。
+ * 示例 1：
+ * <p>
+ * 输入：nums = [4,1,4,6]
+ * 输出：[1,6] 或 [6,1]
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,10,4,1,4,3,3]
+ * 输出：[2,10] 或 [10,2]
+ */
+public class Day22_singleNumbers {
+    /**
+     * 拆分 nums 为两个子数组
+     * 相同的数异或为0，不同的异或为1。0和任何数异或等于这个数本身。
+     *
+     * @param nums
+     * @return
+     */
+    public int[] singleNumbers(int[] nums) {
+        int x = 0, y = 0, n = 0, m = 1;
+        for (int num : nums) {  // 1. 遍历异或
+            n ^= num;
+        }
+
+        while ((n & m) == 0) { // 2. 循环左移，计算 m
+            m <<= 1;
+        }
+        for (int num : nums) { // 3. 遍历 nums 分组
+            if ((num ^ m) != 0) x ^= num; // 4. 当 num & m != 0 , 划分至子数组 2
+            else y ^= num;                // 4. 当 num & m == 0
+        }
+        return new int[]{x, y};           // 5. 返回出现一次的数字
+    }
+}
