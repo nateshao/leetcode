fa447eedc902afe93b2557be1c41c3313ab3f3ef
nateshao <shaotongjie@gmail.com>
2022-07-24 23:34:52 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day27/Codec.java
@@ -0,0 +1,85 @@
+package com.nateshao.sword_offer3.day27;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/24 20:36
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Codec {
+    // private TreeNode root;
+    // // Encodes a tree to a single string.
+    // public String serialize(TreeNode root) {
+    //     this.root = root;
+    //     return null;
+    // }
+
+    // // Decodes your encoded data to tree.
+    // public TreeNode deserialize(String data) {
+    //     return root;
+    // }
+
+
+    public String serialize(TreeNode root) {
+        if (root == null) return "[]";
+        StringBuilder res = new StringBuilder("[");
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                res.append(node.val + ",");
+                queue.add(node.left);
+                queue.add(node.right);
+            } else res.append("null,");
+        }
+        res.deleteCharAt(res.length() - 1);
+        res.append("]");
+        return res.toString();
+    }
+
+    public TreeNode deserialize(String data) {
+        if (data.equals("[]")) return null;
+        String[] vals = data.substring(1, data.length() - 1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+//        Queue<TreeNode> queue = new LinkedList<>() {{
+//            add(root);
+//
+//       }};
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!vals[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if (!vals[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
--- algo-notes/src/main/java/com/nateshao/sword_offer3/day27/Day27_maxSlidingWindow.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day27/Day27_maxSlidingWindow.java
@@ -97,7 +97,6 @@ public class Day27_maxSlidingWindow {
                 res[j++] = nums[queue.peek()];
             }
         }
-
         return res;
     }
 
