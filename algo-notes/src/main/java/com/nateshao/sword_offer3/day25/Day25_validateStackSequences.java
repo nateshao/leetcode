56a6a70f6b6f123324da1b55ea0931c4acb1e382
nateshao <shaotongjie@gmail.com>
2022-07-24 11:14:32 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/sword_offer3/day25/Day25_validateStackSequences.java
@@ -0,0 +1,39 @@
+package com.nateshao.sword_offer3.day25;
+
+import java.util.Stack;
+
+/**
+ * @date Created by 邵桐杰 on 2022/7/24 11:02
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Day25_validateStackSequences {
+    /**
+     * 比较入栈与出栈的值是否相等
+     * 初始化： 辅助栈 stack ，弹出序列的索引 index ；
+     * 遍历压栈序列： 各元素记为 num ；
+     * 元素 num 入栈；
+     * 循环出栈：若 stack 的栈顶元素 = 弹出序列元素 popped[i]，则执行出栈与 i++；
+     * 返回值： 若 stack为空，则此弹出序列合法。
+     *
+     * @param pushed
+     * @param popped
+     * @return
+     */
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int index = 0;
+        for (int num : pushed) {
+            stack.push(num); // 入栈
+            while (!stack.isEmpty() && stack.peek() == popped[index]) { // 循环判断与出栈
+                stack.pop(); // 出栈
+                index++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
