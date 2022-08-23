399b4bde179f00cb25b6a41d434cc7f1a080456c
nateshao <shaotongjie@gmail.com>
2022-08-23 23:04:51 +0800

    算法，我待你如初恋

--- algo-notes/src/main/java/com/nateshao/sword_offer2/code_59/Solution.java
+++ algo-notes/src/main/java/com/nateshao/sword_offer2/code_59/Solution.java
@@ -11,7 +11,7 @@ import java.util.Deque;
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
  * Description: https://leetcode.cn/problems/dui-lie-de-zui-da-zhi-lcof/
- * 面试题59 - II. 队列的最大值
+ * 剑指 Offer 59 - II. 队列的最大值
  * 请定义一个队列并实现函数 max_value 得到队列里的最大值，要求函数max_value、push_back 和 pop_front 的均摊时间复杂度都是O(1)。
  *
  * 若队列为空，pop_front 和 max_value 需要返回 -1
