75cdf20fa594aca8e0a570ee0b5ec41ad969aa5b
nateshao <shaotongjie@gmail.com>
2022-08-27 22:54:28 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/test/meituan_08_27/Main.java
@@ -0,0 +1,28 @@
+package com.nateshao.test.meituan_08_27;
+
+/**
+ * @date Created by 邵桐杰 on 2022/8/27 16:04
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ * 小美在摆弄她的字符串。最近小团送了小美一个特殊字符 ' * '，这个字符可以和其他所有字符匹配，
+ * 除了这个字符外，其他字符只能自己和自己匹配。
+ * 小美先前有一个字符串S，长度为n，现在她又新组合了一个可有特殊字符 ' * ' 的字符串s，
+ * 长度为m。小美想知道有多少个位置 i，使得S[i+j]与s[j]对于1≤j≤m均能匹配上？
+ * 其中X[y]代表字符串X中第y位的字符。
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int getNum(){
+
+        return 0;
+    }
+
+
+}
---
+++ algo-notes/src/test/java/com/nateshao/test/StringTest.java
@@ -0,0 +1,18 @@
+package com.nateshao.test;
+
+
+/**
+ * @date Created by 邵桐杰 on 2022/8/27 13:08
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "123";
+
+    }
+}
