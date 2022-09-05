acf097be4968a8e1e59e53eefc9c8f1dd8802f23
nateshao <shaotongjie@gmail.com>
2022-09-05 15:20:34 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/main/java/com/nateshao/hot100/code_two_pointers/code_141_hasCycle/Solution.java
@@ -0,0 +1,63 @@
+package com.nateshao.hot100.code_two_pointers.code_141_hasCycle;
+
+/**
+ * @date Created by 邵桐杰 on 2022/9/5 15:07
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: https://leetcode.cn/problems/linked-list-cycle/
+ * 141. 环形链表
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+ * 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
+ * <p>
+ * 注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+ * <p>
+ * 示例 1：
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 示例 2：
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 示例 3：
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ */
+public class Solution {
+    /**
+     * 定义快慢指针
+     * 快指针走2步，慢指针走1步，如果快指针==慢指针，说明快指针走了一圈
+     * 说明有环，否则则无环。
+     *
+     * @param head
+     * @return
+     */
+    public boolean hasCycle(ListNode head) {
+        // 定义快慢指针
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
