792c600e8e1de92206a8d9325e740695bf77afd6
nateshao <shaotongjie@gmail.com>
2022-10-02 13:01:17 +0800

    算法，我待你如初恋

--- algo-notes/src/test/java/com/nateshao/sword_offer2/Code_50_firstUniqChar/SolutionTest.java
+++ algo-notes/src/test/java/com/nateshao/sword_offer2/Code_50_firstUniqChar/SolutionTest.java
@@ -11,10 +11,10 @@ import static org.junit.Assert.*;
  * @博客 https://nateshao.gitlab.io
  * @GitHub https://github.com/nateshao
  * @Gitee https://gitee.com/nateshao
- * Description:
+ * Description: 剑指 Offer 50. 第一个只出现一次的字符
  */
 public class SolutionTest {
-    /**
+    /** hashmap统计
      * 如果s为空，return ' '
      * map 创建hashmap<Character,Boolean>
      * s转换成字符数组
@@ -34,4 +34,21 @@ public class SolutionTest {
         return ' ';
     }
 
+    /**
+     * 创建‘a'-'z'的字典
+     * 第一次遍历，将字符统计到字典数组
+     * 第二次遍历，从字典数组获取次数
+     * @param s
+     * @return
+     */
+    public char firstUniqChar2(String s) {
+        if (s.equals("")) return ' ';
+        //创建‘a'-'z'的字典
+        int[] target = new int[26];
+        //第一次遍历，将字符统计到字典数组
+        for (int i = 0; i < s.length(); i++) target[s.charAt(i) - 'a']++;
+        //第二次遍历，从字典数组获取次数
+        for (int i = 0; i < s.length(); i++) if (target[s.charAt(i) - 'a'] == 1) return s.charAt(i);
+        return ' ';
+    }
 }
