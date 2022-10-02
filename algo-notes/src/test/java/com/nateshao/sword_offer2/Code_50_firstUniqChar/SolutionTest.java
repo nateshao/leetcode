f818e377ca93405d04346541c689852d629f86a9
nateshao <shaotongjie@gmail.com>
2022-10-02 12:48:04 +0800

    算法，我待你如初恋

---
+++ algo-notes/src/test/java/com/nateshao/sword_offer2/Code_50_firstUniqChar/SolutionTest.java
@@ -0,0 +1,37 @@
+package com.nateshao.sword_offer2.Code_50_firstUniqChar;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * @date Created by 邵桐杰 on 2022/10/2 12:36
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class SolutionTest {
+    /**
+     * 如果s为空，return ' '
+     * map 创建hashmap<Character,Boolean>
+     * s转换成字符数组
+     * 遍历字符数组，如果map添加key的c，map不包含key的c
+     * 遍历字符数组，如果map有c，return c；
+     * 如果都没找到，就返回为' '
+     *
+     * @param s
+     * @return
+     */
+    public char firstUniqChar(String s) {
+        if (s.isEmpty()) return ' ';
+        HashMap<Character, Boolean> map = new HashMap<>();
+        char[] sc = s.toCharArray();
+        for (char c : sc) map.put(c, !map.containsKey(c));
+        for (char c : sc) if (map.get(c)) return c;
+        return ' ';
+    }
+
+}
