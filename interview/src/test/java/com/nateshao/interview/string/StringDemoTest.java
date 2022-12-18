dda98f1c5762105d893424d2cbcb54c3ad747862
nateshao <shaotongjie@gmail.com>
2022-12-18 23:03:48 +0800

    算法，我爱你

--- interview/src/test/java/com/nateshao/interview/string/StringDemoTest.java
+++ interview/src/test/java/com/nateshao/interview/string/StringDemoTest.java
@@ -16,6 +16,11 @@ import org.springframework.boot.test.context.SpringBootTest;
  */
 @SpringBootTest
 public class StringDemoTest {
+    /**
+     * String s1 = “1”;创建了1个"1"对象，在常量池。
+     * String s2 = new String(“1”);创建了两个"1"对象，一个在常量池，一个在heap区。
+     * String s3 = “1”+“1”;创建了一个"11"对象，不在常量池，在heap区。
+     */
     @Test
     public void demo() {
         String a = "we";
@@ -26,4 +31,18 @@ public class StringDemoTest {
         String f = c + d;
         System.out.println("f = " + f);
     }
+
+    @Test
+    public void demo2() {
+        String s = new String("1");
+        s.intern();
+        String s2 = "1";
+        System.out.println(s == s2);
+
+        String s3 = new String("1") + new String("1");
+        s3.intern();
+        String s4 = "11";
+        System.out.println(s3 == s4);
+    }
+
 }
