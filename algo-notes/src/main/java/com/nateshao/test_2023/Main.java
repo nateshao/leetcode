235e2ea38385afca476cd31928d5d5930a1d7016
nateshao <shaotongjie@gmail.com>
2023-02-11 11:55:20 +0800

    算法，我爱你

---
+++ algo-notes/src/main/java/com/nateshao/test_2023/Main.java
@@ -0,0 +1,41 @@
+package com.nateshao.test_2023;
+
+/**
+ * @date Created by 邵桐杰 on 2023/2/11 11:48
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    // 数组遍历
+    void traverse(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+
+        }
+    }
+
+    // 链表遍历：1.迭代；2.递归
+    // 1.迭代
+    void listNodeTraverse(ListNode head) {
+        for (ListNode p = head; p != null; p = p.next) {
+            // 迭代访问p.val
+        }
+    }
+
+    // 递归访问
+    void diguiTraverse(ListNode head) {
+        diguiTraverse(head.next);
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+    }
+}
