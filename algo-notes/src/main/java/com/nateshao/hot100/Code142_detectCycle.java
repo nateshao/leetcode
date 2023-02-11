a4ed290ff48ab0bd607f4ebfd58e128c87c76843
nateshao <shaotongjie@gmail.com>
2023-02-12 01:06:26 +0800

    算法，我爱你

---
+++ algo-notes/src/main/java/com/nateshao/hot100/Code142_detectCycle.java
@@ -0,0 +1,44 @@
+package com.nateshao.hot100;
+
+/**
+ * @date Created by 邵桐杰 on 2023/2/12 1:03
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description:
+ */
+public class Code142_detectCycle {
+
+    /**
+     * 双指针
+     *
+     * @param head
+     * @return
+     */
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) break;
+        }
+        if (fast == null && fast.next == null) return null;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
