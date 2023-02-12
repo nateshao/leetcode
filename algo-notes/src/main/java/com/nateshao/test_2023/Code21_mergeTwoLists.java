e8d213f78bd75e91cb51dd6b88794e503e2ffcec
nateshao <shaotongjie@gmail.com>
2023-02-12 22:00:51 +0800

    算法，我爱你

--- algo-notes/src/main/java/com/nateshao/test_2023/Code21_mergeTwoLists.java
+++ algo-notes/src/main/java/com/nateshao/test_2023/Code21_mergeTwoLists.java
@@ -71,7 +71,6 @@ public class Code21_mergeTwoLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if (list1 == null) return list2;
         if (list2 == null) return list1;
-        if (list1 == list2) return list1;
         if (list1.val < list2.val) {
             list1.next = mergeTwoLists(list1.next, list2);
             return list1;
