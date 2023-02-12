aca435e82df4ae80a98baa5771fd7fbb68f9a46b
nateshao <shaotongjie@gmail.com>
2023-02-12 23:13:23 +0800

    算法，我爱你

---
+++ algo-notes/src/main/java/com/nateshao/test_2023/Code23_mergeKLists.java
@@ -0,0 +1,42 @@
+package com.nateshao.test_2023;
+
+import java.util.PriorityQueue;
+
+/**
+ * @date Created by 邵桐杰 on 2023/2/12 22:27
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 23. 合并K个升序链表
+ * https://leetcode.cn/problems/merge-k-sorted-lists/
+ */
+public class Code23_mergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        // 虚拟头结点
+        ListNode dummy = new ListNode(-1);
+        ListNode p = dummy;
+        // 优先级队列，最小堆
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+                lists.length, (a, b) -> (a.val - b.val));
+        // 将 k 个链表的头结点加入最小堆
+        for (ListNode head : lists) {
+            if (head != null)
+                pq.add(head);
+        }
+
+        while (!pq.isEmpty()) {
+            // 获取最小节点，接到结果链表中
+            ListNode node = pq.poll();
+            p.next = node;
+            if (node.next != null) {
+                pq.add(node.next);
+            }
+            // p 指针不断前进
+            p = p.next;
+        }
+        return dummy.next;
+    }
+}
---
+++ algo-notes/src/main/java/com/nateshao/test_2023/Code_offer22_getKthFromEnd.java
@@ -0,0 +1,36 @@
+package com.nateshao.test_2023;
+
+/**
+ * @date Created by 邵桐杰 on 2023/2/12 23:02
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 剑指 Offer 22. 链表中倒数第k个节点
+ * https://leetcode.cn/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
+ */
+public class Code_offer22_getKthFromEnd {
+
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode fast = head, slow = head;
+        for (int i = 0; i < k; i++) {
+            fast = fast.next;
+        }
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
