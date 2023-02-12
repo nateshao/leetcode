75a5a8d27bb59e129ae3282bf5e93c800421f563
nateshao <shaotongjie@gmail.com>
2023-02-12 22:00:26 +0800

    算法，我爱你

---
+++ algo-notes/src/main/java/com/nateshao/test_2023/Code21_mergeTwoLists.java
@@ -0,0 +1,102 @@
+package com.nateshao.test_2023;
+
+/**
+ * @date Created by 邵桐杰 on 2023/2/12 20:08
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 21. 合并两个有序链表
+ * https://leetcode.cn/problems/merge-two-sorted-lists/
+ */
+public class Code21_mergeTwoLists {
+
+    /**
+     * 递归
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeTwoLists3(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists3(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists3(list1, list2.next);
+            return list2;
+        }
+
+    }
+
+    /**
+     * 双指针
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        ListNode res = new ListNode(-1), p = res;
+        ListNode p1 = list1, p2 = list2;
+        while (p1 != null && p2 != null) {
+            if (p1.val > p2.val) {
+                p.next = p2;
+                p2 = p2.next;
+            } else {
+                p.next = p1;
+                p1 = p1.next;
+            }
+            p = p.next;
+        }
+        if (p1 != null) {
+            p.next = p1;
+        }
+        if (p2 != null) p.next = p2;
+        return res.next;
+
+    }
+
+
+    /**
+     * 递归
+     *
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+        if (list1 == list2) return list1;
+        if (list1.val < list2.val) {
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
