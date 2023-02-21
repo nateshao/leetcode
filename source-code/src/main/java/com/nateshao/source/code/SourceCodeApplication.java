329b42855f93c6df1391dee3b2f060a4a18e078e
nateshao <shaotongjie@gmail.com>
2023-02-21 13:24:12 +0800

    算法，我爱你

--- source-code/src/main/java/com/nateshao/source/code/SourceCodeApplication.java
+++ source-code/src/main/java/com/nateshao/source/code/SourceCodeApplication.java
@@ -1,7 +1,7 @@
-package com.nateshao.interview.string;
+package com.nateshao.source.code;
 
 /**
- * @date Created by 邵桐杰 on 2022/12/18 22:56
+ * @date Created by 邵桐杰 on 2023/2/21 13:22
  * @微信公众号 千羽的编程时光
  * @个人网站 www.nateshao.cn
  * @博客 https://nateshao.gitlab.io
@@ -9,7 +9,7 @@ package com.nateshao.interview.string;
  * @Gitee https://gitee.com/nateshao
  * Description:
  */
-public class StringDemo {
+public class SourceCodeApplication {
     public static void main(String[] args) {
 
     }
