6422031949655e52a56a7701103837650f27db08
nateshao <shaotongjie@gmail.com>
2023-02-21 13:34:53 +0800

    算法，我爱你

---
+++ source-code/src/main/java/com/nateshao/source/code/java_base/ScannerDemo.java
@@ -0,0 +1,21 @@
+package com.nateshao.source.code.java_base;
+
+import java.util.Scanner;
+
+/**
+ * @date Created by 邵桐杰 on 2023/2/21 13:25
+ * @微信公众号 千羽的编程时光
+ * @个人网站 www.nateshao.cn
+ * @博客 https://nateshao.gitlab.io
+ * @GitHub https://github.com/nateshao
+ * @Gitee https://gitee.com/nateshao
+ * Description: 键盘录入
+ */
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        System.out.println(b + c);
+    }
+}
