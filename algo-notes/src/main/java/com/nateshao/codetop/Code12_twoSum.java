61a5929fb59812cee71dfd4d73c2592343568f6d
邵桐杰 <shaotj@300624.cn>
2024-12-21 11:23:07 +0800

    重新整理

---
+++ algo-notes/src/main/java/com/nateshao/codetop/Code12_twoSum.java
@@ -0,0 +1,28 @@
+package com.nateshao.codetop;
+
+import java.util.HashMap;
+
+public class Code12_twoSum {
+    /**
+     * 1. 两数之和
+     * https://leetcode.cn/problems/two-sum/description/
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        // 维护 val -> index 的映射
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            // 查表，看看是否有能和 nums[i] 凑出 target 的元素
+            int need = target - nums[i];
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+            // 存入 val -> index 的映射
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
