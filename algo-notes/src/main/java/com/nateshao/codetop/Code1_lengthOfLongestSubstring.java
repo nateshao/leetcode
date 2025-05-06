d774ccdf011dc1539ff85202e8198b66a1ff800a
邵桐杰 <shaotj@300624.cn>
2025-05-06 11:30:46 +0800

    LRU

--- algo-notes/src/main/java/com/nateshao/codetop/Code1_lengthOfLongestSubstring.java
+++ algo-notes/src/main/java/com/nateshao/codetop/Code1_lengthOfLongestSubstring.java
@@ -242,6 +242,54 @@ public class Code1_lengthOfLongestSubstring {
         return res;
     }
 
+    /**
+     * hashset
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring9(String s) {
+        if (s == null || s.length() == 0) return 0;
+        HashSet<Integer> set = new HashSet<>();
+        int left = 0, right = 0;
+        int res = 0;
+        while (right < s.length()) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(right);
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+            }
+            res = Math.max(res, set.size());
+        }
+        return res;
+    }
+
+    /**
+     * hashmap
+     *
+     * @param s
+     * @return
+     */
+    public static int lengthOfLongestSubstring10(String s) {
+        if (s == null || s.length() == 0) return 0;
+        HashMap<Character, Integer> window = new HashMap<>();
+        int left = 0, right = 0;
+        int res = 0;
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            right++;
+            window.put(c, window.getOrDefault(c, 0));
+            while (window.get(c) > 1) {
+                char d = s.charAt(left);
+                left++;
+                window.put(d, window.get(d) - 1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int abcabcbb = lengthOfLongestSubstring7("abcabcbb");
         System.out.println(abcabcbb);
